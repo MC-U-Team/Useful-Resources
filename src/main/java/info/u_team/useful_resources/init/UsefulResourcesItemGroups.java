a45688e90408a64ac75d7bce39dbf1574d758afa
HyCraftHD <hycrafthd@live.de>
2019-07-10 21:12:36 +0200

    Change icon

--- src/main/java/info/u_team/useful_resources/init/UsefulResourcesItemGroups.java
+++ src/main/java/info/u_team/useful_resources/init/UsefulResourcesItemGroups.java
@@ -2,10 +2,10 @@ package info.u_team.useful_resources.init;
 
 import info.u_team.u_team_core.itemgroup.UItemGroup;
 import info.u_team.useful_resources.UsefulResourcesMod;
-import net.minecraft.item.Items;
+import info.u_team.useful_resources.type.Resources;
 
 public class UsefulResourcesItemGroups {
 	
-	public static final UItemGroup GROUP = new UItemGroup(UsefulResourcesMod.MODID, "group", () -> Items.ACACIA_BOAT);
+	public static final UItemGroup GROUP = new UItemGroup(UsefulResourcesMod.MODID, "group", () -> Resources.COPPER.getBlocks().getOre());
 	
 }
