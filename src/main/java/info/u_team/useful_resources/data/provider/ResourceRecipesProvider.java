a087d99b2e1955faa6c70fdda78e7a0acd1415a9
HyCraftHD <hycrafthd@live.de>
2019-07-29 19:33:22 +0200

    Fixed last commit using wrong generated data

--- src/main/java/info/u_team/useful_resources/data/provider/ResourceRecipesProvider.java
+++ src/main/java/info/u_team/useful_resources/data/provider/ResourceRecipesProvider.java
@@ -63,10 +63,10 @@ public class ResourceRecipesProvider extends CommonProvider {
 		ShapedRecipeBuilder.shapedRecipe(items.getItem(ROD)).key('#', items.getTag(INGOT)).patternLine("#").patternLine("#").addCriterion("has_at_least_2_" + name + "_ingot", hasItem(IntBound.atLeast(9), items.getTag(INGOT))).build(consumer, createLocation(resource, "crafting/rod_from_ingot"));
 		ShapedRecipeBuilder.shapedRecipe(items.getItem(INGOT)).key('#', items.getTag(NUGGET)).patternLine("###").patternLine("###").patternLine("###").addCriterion("has_at_least_9_" + name + "_nugget", hasItem(IntBound.atLeast(9), items.getTag(NUGGET))).build(consumer, createLocation(resource, "crafting/ingot_from_nugget"));
 		
-		ShapelessRecipeBuilder.shapelessRecipe(items.getItem(NUGGET), 9).addIngredient(items.getTag(INGOT)).addCriterion("has_" + name + "_nugget", hasItem(items.getTag(INGOT))).build(consumer, createLocation(resource, "crafting/nugget_from_ingot"));
-		ShapelessRecipeBuilder.shapelessRecipe(items.getItem(INGOT), 9).addIngredient(blocks.getTag(BLOCK)).addCriterion("has_" + name + "_block", hasItem(items.getTag(INGOT))).build(consumer, createLocation(resource, "crafting/ingot_from_block"));
-		ShapelessRecipeBuilder.shapelessRecipe(items.getItem(INGOT), 4).addIngredient(items.getTag(GEAR)).addCriterion("has_" + name + "_gear", hasItem(items.getTag(INGOT))).build(consumer, createLocation(resource, "crafting/ingot_from_gear"));
-		ShapelessRecipeBuilder.shapelessRecipe(items.getItem(INGOT), 2).addIngredient(items.getTag(ROD)).addCriterion("has_" + name + "_rod", hasItem(items.getTag(INGOT))).build(consumer, createLocation(resource, "crafting/ingot_from_rod"));
+		ShapelessRecipeBuilder.shapelessRecipe(items.getItem(NUGGET), 9).addIngredient(items.getTag(INGOT)).addCriterion("has_" + name + "_nugget", hasItem(items.getTag(NUGGET))).build(consumer, createLocation(resource, "crafting/nugget_from_ingot"));
+		ShapelessRecipeBuilder.shapelessRecipe(items.getItem(INGOT), 9).addIngredient(blocks.getTag(BLOCK)).addCriterion("has_" + name + "_block", hasItem(blocks.getTag(BLOCK))).build(consumer, createLocation(resource, "crafting/ingot_from_block"));
+		ShapelessRecipeBuilder.shapelessRecipe(items.getItem(INGOT), 4).addIngredient(items.getTag(GEAR)).addCriterion("has_" + name + "_gear", hasItem(items.getTag(GEAR))).build(consumer, createLocation(resource, "crafting/ingot_from_gear"));
+		ShapelessRecipeBuilder.shapelessRecipe(items.getItem(INGOT), 2).addIngredient(items.getTag(ROD)).addCriterion("has_" + name + "_rod", hasItem(items.getTag(ROD))).build(consumer, createLocation(resource, "crafting/ingot_from_rod"));
 	}
 	
 	@Override
