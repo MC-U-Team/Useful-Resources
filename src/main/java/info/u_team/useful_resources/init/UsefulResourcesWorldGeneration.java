1ac953c259a5e5883ee5bf47974724a53e0fc9c6
HyCraftHD <hycrafthd@live.de>
2019-11-16 17:52:12 +0100

    Remove proxies

--- src/main/java/info/u_team/useful_resources/init/UsefulResourcesWorldGeneration.java
+++ src/main/java/info/u_team/useful_resources/init/UsefulResourcesWorldGeneration.java
@@ -1,5 +1,6 @@
 package info.u_team.useful_resources.init;
 
+import info.u_team.useful_resources.UsefulResourcesMod;
 import info.u_team.useful_resources.api.resource.IResourceBlocks;
 import info.u_team.useful_resources.api.resource.config.generation.*;
 import info.u_team.useful_resources.config.CommonConfig;
@@ -9,11 +10,17 @@ import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.GenerationStage.Decoration;
 import net.minecraft.world.gen.feature.*;
 import net.minecraft.world.gen.placement.Placement;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.registries.ForgeRegistries;
 
+@EventBusSubscriber(modid = UsefulResourcesMod.MODID, bus = Bus.MOD)
 public class UsefulResourcesWorldGeneration {
 	
-	public static void setup() {
+	@SubscribeEvent
+	public static void register(FMLCommonSetupEvent event) {
 		final CommonConfig config = CommonConfig.getInstance();
 		if (!config.worldGenerationEnabled.get()) {
 			return;
