d80745b72f798378bd393d841083c7f28b2f38c6
HyCraftHD <hycrafthd@live.de>
2019-11-16 17:52:12 +0100

    Remove proxies

--- src/main/java/info/u_team/useful_resources/UsefulResourcesMod.java
+++ src/main/java/info/u_team/useful_resources/UsefulResourcesMod.java
@@ -1,33 +1,17 @@
 package info.u_team.useful_resources;
 
-import info.u_team.u_team_core.api.IModProxy;
-import info.u_team.useful_resources.proxy.*;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.DistExecutor;
+import info.u_team.useful_resources.config.CommonConfig;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.*;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.config.ModConfig.Type;
 
 @Mod(UsefulResourcesMod.MODID)
 public class UsefulResourcesMod {
 	
 	public static final String MODID = "usefulresources";
 	
-	public static final IModProxy PROXY = DistExecutor.runForDist(() -> ClientProxy::new, () -> CommonProxy::new);
-	
 	public UsefulResourcesMod() {
-		FMLJavaModLoadingContext.get().getModEventBus().register(this);
-		PROXY.construct();
-	}
-	
-	@SubscribeEvent
-	public void setup(FMLCommonSetupEvent event) {
-		PROXY.setup();
-	}
-	
-	@SubscribeEvent
-	public void ready(FMLLoadCompleteEvent event) {
-		PROXY.complete();
+		ModLoadingContext.get().registerConfig(Type.COMMON, CommonConfig.CONFIG, UsefulResourcesMod.MODID + "/common.toml");
 	}
 	
 }
