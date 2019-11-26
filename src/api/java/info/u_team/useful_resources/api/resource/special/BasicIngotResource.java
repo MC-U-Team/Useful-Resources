b4285cb9728501e949d5e8786e6fce8a2ea79b37
HyCraftHD <hycrafthd@live.de>
2019-11-26 15:02:09 +0100

    Use some basic ingor resources

--- src/api/java/info/u_team/useful_resources/api/resource/special/BasicIngotResource.java
+++ src/api/java/info/u_team/useful_resources/api/resource/special/BasicIngotResource.java
@@ -4,14 +4,20 @@ import static info.u_team.useful_resources.api.resource.CommonResourceBuilder.*;
 import static info.u_team.useful_resources.api.type.BlockResourceType.*;
 import static info.u_team.useful_resources.api.type.ItemResourceType.*;
 
+import info.u_team.u_team_core.api.IToolMaterial;
+import info.u_team.useful_resources.api.material.*;
 import info.u_team.useful_resources.api.resource.*;
 import info.u_team.useful_resources.api.type.ItemResourceType;
-import net.minecraft.item.Rarity;
+import net.minecraft.item.*;
+import net.minecraft.item.crafting.Ingredient;
 
 public class BasicIngotResource extends Resource {
 	
+	private final Rarity rarity;
+	
 	public BasicIngotResource(String name, int color, Rarity rarity, int harvestLevel, float baseHardness) {
 		super(name, color, ItemResourceType.INGOT);
+		this.rarity = rarity;
 		addFeature(createOre(ORE, rarity, harvestLevel, baseHardness, baseHardness * 1.5F));
 		addFeature(createOre(NETHER_ORE, rarity, harvestLevel, baseHardness * 0.75F, baseHardness * 1.25F));
 		addFeature(createBasicBlock(BLOCK, rarity, harvestLevel, baseHardness * 2, baseHardness * 4));
@@ -23,6 +29,21 @@ public class BasicIngotResource extends Resource {
 		addFeature(createBasicItem(DENSE_PLATE, rarity));
 		addFeature(createBasicItem(GEAR, rarity));
 		addFeature(createBasicItem(ROD, rarity));
+	}
+	
+	public BasicIngotResource addTools(IToolMaterial toolMaterial) {
+		addFeature(createTools(rarity, new WrappedToolMaterial(toolMaterial, () -> Ingredient.fromItems(getItems().get(getRepairType())))));
+		return this;
+	}
+	
+	public BasicIngotResource addArmor(IArmorMaterial armorMaterial) {
+		addFeature(createArmor(rarity, new WrappedArmorMaterial(armorMaterial, () -> Ingredient.fromItems(getItems().get(getRepairType())))));
+		return this;
+	}
+	
+	public BasicIngotResource addHorseArmor(int armorPoints) {
+		addFeature(createHorseArmor(rarity, armorPoints));
+		return this;
 	}
 	
 	@Override
