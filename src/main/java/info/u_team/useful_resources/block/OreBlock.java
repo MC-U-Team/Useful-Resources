fe44f83b7ef5a9aec41de6853136bb046d2263b7
HyCraftHD <hycrafthd@live.de>
2019-11-28 00:39:51 +0100

    These methods needs to be modified

--- src/main/java/info/u_team/useful_resources/block/OreBlock.java
+++ src/main/java/info/u_team/useful_resources/block/OreBlock.java
@@ -7,10 +7,13 @@ import info.u_team.u_team_core.block.UBlock;
 import info.u_team.useful_resources.init.UsefulResourcesItemGroups;
 import net.minecraft.block.*;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.particle.ParticleManager;
+import net.minecraft.entity.*;
 import net.minecraft.item.*;
 import net.minecraft.util.BlockRenderLayer;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IWorldReader;
+import net.minecraft.util.math.*;
+import net.minecraft.world.*;
+import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.common.ToolType;
 
 public class OreBlock extends UBlock {
@@ -30,5 +33,25 @@ public class OreBlock extends UBlock {
 	@Override
 	public BlockRenderLayer getRenderLayer() {
 		return BlockRenderLayer.CUTOUT;
+	}
+	
+	@Override
+	public boolean addLandingEffects(BlockState state1, ServerWorld worldserver, BlockPos pos, BlockState state2, LivingEntity entity, int numberOfParticles) {
+		return super.addLandingEffects(state1, worldserver, pos, state2, entity, numberOfParticles);
+	}
+	
+	@Override
+	public boolean addRunningEffects(BlockState state, World world, BlockPos pos, Entity entity) {
+		return super.addRunningEffects(state, world, pos, entity);
+	}
+	
+	@Override
+	public boolean addHitEffects(BlockState state, World worldObj, RayTraceResult target, ParticleManager manager) {
+		return super.addHitEffects(state, worldObj, target, manager);
+	}
+	
+	@Override
+	public boolean addDestroyEffects(BlockState state, World world, BlockPos pos, ParticleManager manager) {
+		return super.addDestroyEffects(state, world, pos, manager);
 	}
 }
