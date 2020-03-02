6289866e53cec702cad3ff624a8594bd0f8fa99d
HyCraftHD <hycrafthd@live.de>
2020-03-02 14:28:59 +0100

    Register new particle type

---
+++ src/main/java/info/u_team/useful_resources/init/UsefulResourcesParticleTypes.java
@@ -0,0 +1,22 @@
+package info.u_team.useful_resources.init;
+
+import info.u_team.move_to_u_team_core.UParticleType;
+import info.u_team.u_team_core.util.registry.BaseRegistryUtil;
+import info.u_team.useful_resources.UsefulResourcesMod;
+import net.minecraft.particles.*;
+import net.minecraftforge.event.RegistryEvent.Register;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+
+@EventBusSubscriber(modid = UsefulResourcesMod.MODID, bus = Bus.MOD)
+public class UsefulResourcesParticleTypes {
+	
+	public static final UParticleType<BlockParticleData> COLORED_OVERLAY_BLOCK = new UParticleType<>("colored_overlay_block", false, BlockParticleData.DESERIALIZER);
+	
+	@SubscribeEvent
+	public static void register(Register<ParticleType<?>> event) {
+		BaseRegistryUtil.getAllGenericRegistryEntriesAndApplyNames(UsefulResourcesMod.MODID, ParticleType.class).forEach(event.getRegistry()::register);
+	}
+	
+}
