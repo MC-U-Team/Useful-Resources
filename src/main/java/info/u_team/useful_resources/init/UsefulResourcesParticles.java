6bdcc825d796f0b79c1e79e216ebc0be2df3ceaa
HyCraftHD <hycrafthd@live.de>
2020-03-02 14:37:48 +0100

    Register particle (basic digging currently)

---
+++ src/main/java/info/u_team/useful_resources/init/UsefulResourcesParticles.java
@@ -0,0 +1,22 @@
+package info.u_team.useful_resources.init;
+
+import info.u_team.useful_resources.UsefulResourcesMod;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.particle.*;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.ParticleFactoryRegisterEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+
+@EventBusSubscriber(modid = UsefulResourcesMod.MODID, bus = Bus.MOD, value = Dist.CLIENT)
+public class UsefulResourcesParticles {
+	
+	@SubscribeEvent
+	public static void register(ParticleFactoryRegisterEvent event) {
+		final ParticleManager manager = Minecraft.getInstance().particles;
+		
+		manager.registerFactory(UsefulResourcesParticleTypes.COLORED_OVERLAY_BLOCK, new DiggingParticle.Factory());
+	}
+	
+}
