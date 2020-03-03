af1260915c2cb1e803f9322d0fc333858a17bd6e
HyCraftHD <hycrafthd@live.de>
2020-03-03 18:02:14 +0100

    Generate base item models

--- src/main/java/info/u_team/useful_resources/data/provider/ResourceItemModelsProvider.java
+++ src/main/java/info/u_team/useful_resources/data/provider/ResourceItemModelsProvider.java
@@ -2,8 +2,6 @@ package info.u_team.useful_resources.data.provider;
 
 import info.u_team.u_team_core.data.*;
 import info.u_team.useful_resources.api.ResourceRegistry;
-import net.minecraft.item.TieredItem;
-import net.minecraftforge.client.model.generators.ModelFile.UncheckedModelFile;
 
 public class ResourceItemModelsProvider extends CommonItemModelsProvider {
 	
@@ -13,15 +11,35 @@ public class ResourceItemModelsProvider extends CommonItemModelsProvider {
 	
 	@Override
 	protected void registerModels() {
+		generateBaseModels();
+		
 		ResourceRegistry.getResources().stream().flatMap(resource -> resource.getBlocks().values().stream()).forEach(this::simpleBlock);
-//		
-//		ResourceRegistry.getResources().forEach(resource -> {
-//			resource.getItems().values().forEach(item -> {
-//				final String itemName = item.getRegistryName().getPath();
-//				final String parent = item instanceof TieredItem ? "handheld" : "generated";
-//				
-//				getBuilder(itemName).parent(new UncheckedModelFile("item/" + parent)).texture("layer0", "item/" + resource.getName() + "/" + itemName.replace(resource.getName() + "_", ""));
-//			});
-//		});
+		//
+		// ResourceRegistry.getResources().forEach(resource -> {
+		// resource.getItems().values().forEach(item -> {
+		// final String itemName = item.getRegistryName().getPath();
+		// final String parent = item instanceof TieredItem ? "handheld" : "generated";
+		//
+		// getBuilder(itemName).parent(new UncheckedModelFile("item/" + parent)).texture("layer0", "item/" + resource.getName()
+		// + "/" + itemName.replace(resource.getName() + "_", ""));
+		// });
+		// });
+	}
+	
+	private void generateBaseModels() {
+		// Basic types
+		withExistingParent("base/item/colored_generated_item", "generated") //
+				.texture("layer1", "#colored");
+		
+		withExistingParent("base/item/colored_handheld_item", "handheld") //
+				.texture("layer1", "#colored");
+		
+		withExistingParent("base/item/colored_overlay_generated_item", modLoc("base/item/colored_generated_item")) //
+				.texture("layer0", "#uncolored");
+		
+		withExistingParent("base/item/colored_overlay_handheld_item", modLoc("base/item/colored_handheld_item")) //
+				.texture("layer0", "#uncolored");
+		
+		// Special models for each type
 	}
 }
