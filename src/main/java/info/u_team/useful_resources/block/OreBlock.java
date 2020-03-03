545a73be3b125bcc4e09377d115b2c4df4c61810
HyCraftHD <hycrafthd@live.de>
2020-03-03 03:32:47 +0100

    Add hit effects particle

--- src/main/java/info/u_team/useful_resources/block/OreBlock.java
+++ src/main/java/info/u_team/useful_resources/block/OreBlock.java
@@ -5,15 +5,18 @@ import java.util.function.Function;
 
 import info.u_team.u_team_core.block.UBlock;
 import info.u_team.useful_resources.init.*;
+import info.u_team.useful_resources.particle.ColoredOverlayDiggingParticle;
 import net.minecraft.block.*;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.particle.ParticleManager;
 import net.minecraft.entity.*;
 import net.minecraft.item.*;
 import net.minecraft.particles.BlockParticleData;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.*;
 import net.minecraft.world.*;
 import net.minecraft.world.server.ServerWorld;
+import net.minecraftforge.api.distmarker.*;
 import net.minecraftforge.common.ToolType;
 
 public class OreBlock extends UBlock {
@@ -43,13 +46,57 @@ public class OreBlock extends UBlock {
 		return true;
 	}
 	
+	@OnlyIn(Dist.CLIENT)
 	@Override
-	public boolean addHitEffects(BlockState state, World worldObj, RayTraceResult target, ParticleManager manager) {
-		return super.addHitEffects(state, worldObj, target, manager);
+	public boolean addHitEffects(BlockState state, World world, RayTraceResult result, ParticleManager manager) {
+		if (!(result instanceof BlockRayTraceResult)) {
+			return false;
+		}
+		
+		final BlockRayTraceResult blockResult = (BlockRayTraceResult) result;
+		
+		final BlockPos pos = blockResult.getPos();
+		final Direction side = blockResult.getFace();
+		final int x = pos.getX();
+		final int y = pos.getY();
+		final int z = pos.getZ();
+		
+		final AxisAlignedBB aabb = state.getShape(world, pos).getBoundingBox();
+		
+		double xCoord = (double) x + manager.rand.nextDouble() * (aabb.maxX - aabb.minX - (double) 0.2F) + (double) 0.1F + aabb.minX;
+		double yCoord = (double) y + manager.rand.nextDouble() * (aabb.maxY - aabb.minY - (double) 0.2F) + (double) 0.1F + aabb.minY;
+		double zCoord = (double) z + manager.rand.nextDouble() * (aabb.maxZ - aabb.minZ - (double) 0.2F) + (double) 0.1F + aabb.minZ;
+		if (side == Direction.DOWN) {
+			yCoord = y + aabb.minY - 0.1;
+		}
+		
+		if (side == Direction.UP) {
+			yCoord = y + aabb.maxY + 0.1;
+		}
+		
+		if (side == Direction.NORTH) {
+			zCoord = z + aabb.minZ - 0.1;
+		}
+		
+		if (side == Direction.SOUTH) {
+			zCoord = z + aabb.maxZ + 0.1;
+		}
+		
+		if (side == Direction.WEST) {
+			xCoord = x + aabb.minX - 0.1;
+		}
+		
+		if (side == Direction.EAST) {
+			xCoord = x + aabb.maxX + 0.1;
+		}
+		
+		manager.addEffect((new ColoredOverlayDiggingParticle(world, xCoord, yCoord, zCoord, 0, 0, 0, state)).setBlockPos(pos).multiplyVelocity(0.2F).multipleParticleScaleBy(0.6F));
+		return true;
 	}
 	
+	@OnlyIn(Dist.CLIENT)
 	@Override
 	public boolean addDestroyEffects(BlockState state, World world, BlockPos pos, ParticleManager manager) {
-		return super.addDestroyEffects(state, world, pos, manager);
+		return true;
 	}
 }
