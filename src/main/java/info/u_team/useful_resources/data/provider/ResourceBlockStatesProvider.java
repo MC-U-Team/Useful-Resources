2f0ce2e0ef4c71ccc4d5659396b0e499b0a3edf3
HyCraftHD <hycrafthd@live.de>
2020-03-03 13:58:26 +0100

    Update generator

--- src/main/java/info/u_team/useful_resources/data/provider/ResourceBlockStatesProvider.java
+++ src/main/java/info/u_team/useful_resources/data/provider/ResourceBlockStatesProvider.java
@@ -2,7 +2,9 @@ package info.u_team.useful_resources.data.provider;
 
 import info.u_team.u_team_core.data.*;
 import info.u_team.useful_resources.api.ResourceRegistry;
+import info.u_team.useful_resources.api.resource.data.*;
 import info.u_team.useful_resources.api.type.BlockResourceType;
+import net.minecraft.util.ResourceLocation;
 
 public class ResourceBlockStatesProvider extends CommonBlockStatesProvider {
 	
@@ -12,7 +14,31 @@ public class ResourceBlockStatesProvider extends CommonBlockStatesProvider {
 	
 	@Override
 	protected void registerStatesAndModels() {
-		// Make base blocks
+		generateBaseModels();
+		
+		ResourceRegistry.getResources().forEach(resource -> {
+			resource.getBlocks().forEach((type, block) -> {
+				simpleBlock(block, models().withExistingParent(getPath(block), getBaseModel(type, resource.getDataGeneratorConfigurator())));
+			});
+		});
+	}
+	
+	private ResourceLocation getBaseModel(BlockResourceType type, IDataGeneratorConfigurator dataGeneratorConfigurator) {
+		final String baseModel;
+		final OreType oreType = dataGeneratorConfigurator.getOreType();
+		if (type == BlockResourceType.ORE) {
+			baseModel = oreType.getName() + "_stone_ore";
+		} else if (type == BlockResourceType.NETHER_ORE) {
+			baseModel = oreType.getName() + "_netherrack_ore";
+		} else if (type == BlockResourceType.BLOCK) {
+			baseModel = "block";
+		} else {
+			baseModel = "invalid";
+		}
+		return modLoc("base/" + baseModel);
+	}
+	
+	private void generateBaseModels() {
 		models().withExistingParent("base/colored_block", "block") //
 				.texture("particle", "#colored") //
 				.element() //
@@ -21,7 +47,7 @@ public class ResourceBlockStatesProvider extends CommonBlockStatesProvider {
 				.allFaces((direction, builder) -> {
 					builder.texture("#colored") //
 							.cullface(direction) //
-							.tintindex(0);
+							.tintindex(1);
 				}) //
 				.end();
 		
@@ -41,28 +67,19 @@ public class ResourceBlockStatesProvider extends CommonBlockStatesProvider {
 				.allFaces((direction, builder) -> {
 					builder.texture("#colored") //
 							.cullface(direction) //
-							.tintindex(0);
+							.tintindex(1);
 				}) //
 				.end();
 		
-		models().withExistingParent("base/default_block", modLoc("base/colored_block")) //
-				.texture("colored", "block/default_block");
+		models().withExistingParent("base/block", modLoc("base/colored_block")) //
+				.texture("colored", "block/block");
 		
-		models().withExistingParent("base/default_stone_ore", modLoc("base/colored_overlay_block")) //
-				.texture("uncolored", "block/stone_background") //
-				.texture("colored", "block/default_ore");
+		models().withExistingParent("base/ingot_stone_ore", modLoc("base/colored_overlay_block")) //
+				.texture("uncolored", "block/stone_ingot_ore_background") //
+				.texture("colored", "block/ingot_ore");
 		
-		ResourceRegistry.getResources().forEach(resource -> {
-			resource.getBlocks().forEach((type, block) -> {
-				final String blockName = block.getRegistryName().getPath();
-				// simpleBlock(block, cubeAll(blockName, modLoc("block/" + resource.getName() + "/" +
-				// blockName.replace(resource.getName() + "_", ""))));
-				if (type == BlockResourceType.BLOCK) {
-					simpleBlock(block, models().withExistingParent(blockName, modLoc("base/default_block")));
-				} else {
-					simpleBlock(block, models().withExistingParent(blockName, modLoc("base/default_stone_ore")));
-				}
-			});
-		});
+		models().withExistingParent("base/ingot_netherrack_ore", modLoc("base/colored_overlay_block")) //
+				.texture("uncolored", "block/netherrack_ingot_ore_background") //
+				.texture("colored", "block/ingot_ore");
 	}
 }
