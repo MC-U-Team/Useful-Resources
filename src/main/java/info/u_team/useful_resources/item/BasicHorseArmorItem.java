554ab26ecaa0d9e9d88b7cb10dd184d097cd36de
HyCraftHD <hycrafthd@live.de>
2020-03-03 22:16:58 +0100

    Add horse armor to the item group

--- src/main/java/info/u_team/useful_resources/item/BasicHorseArmorItem.java
+++ src/main/java/info/u_team/useful_resources/item/BasicHorseArmorItem.java
@@ -2,6 +2,7 @@ package info.u_team.useful_resources.item;
 
 import info.u_team.u_team_core.api.registry.IURegistryType;
 import info.u_team.useful_resources.UsefulResourcesMod;
+import info.u_team.useful_resources.init.UsefulResourcesItemGroups;
 import net.minecraft.item.*;
 import net.minecraft.util.ResourceLocation;
 
@@ -10,7 +11,7 @@ public class BasicHorseArmorItem extends HorseArmorItem implements IURegistryTyp
 	protected final String name;
 	
 	public BasicHorseArmorItem(String name, Rarity rarity, int armorPoints) {
-		super(armorPoints, new ResourceLocation(UsefulResourcesMod.MODID, "textures/entity/horse/armor/" + name + ".png"), new Properties());
+		super(armorPoints, new ResourceLocation(UsefulResourcesMod.MODID, "textures/entity/horse/armor/" + name + ".png"), new Properties().group(UsefulResourcesItemGroups.GROUP));
 		this.name = name + "_horse_armor";
 	}
 	
