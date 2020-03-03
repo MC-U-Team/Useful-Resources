6da5901786faa14e20e097a371dda6922f34b502
HyCraftHD <hycrafthd@live.de>
2020-03-03 22:08:50 +0100

    Reenable horse armor

--- src/main/java/info/u_team/useful_resources/resources/Resources.java
+++ src/main/java/info/u_team/useful_resources/resources/Resources.java
@@ -11,7 +11,7 @@ public class Resources {
 	
 	public static final IResource COPPER = new BasicIngotResource("copper", 0xcf6617, Rarity.COMMON, 1, 3) //
 			.setArmor(createArmor(12, new int[] { 2, 5, 6, 2 }, 9)) //
-			// .setHorseArmor(6) //
+			.setHorseArmor(6) //
 			.setTools(createTools(new float[] { 6, 0, 1, 1.5F, 3 }, new float[] { -3.1F, -1, -2.8F, -3, -2.4F }, 2, 200, 5, 1, 14));
 	
 	public static void register() {
