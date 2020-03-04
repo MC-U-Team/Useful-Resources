5b0f48ab4334abbe224e817eea65a7d8017dbcc7
HyCraftHD <hycrafthd@live.de>
2020-03-04 02:49:04 +0100

    Make the horse armor dyeable

--- src/main/java/info/u_team/useful_resources/item/BasicHorseArmorItem.java
+++ src/main/java/info/u_team/useful_resources/item/BasicHorseArmorItem.java
@@ -2,16 +2,17 @@ package info.u_team.useful_resources.item;
 
 import info.u_team.u_team_core.api.registry.IURegistryType;
 import info.u_team.useful_resources.UsefulResourcesMod;
+import info.u_team.useful_resources.api.material.IColoredArmor;
 import info.u_team.useful_resources.init.UsefulResourcesItemGroups;
 import net.minecraft.item.*;
 import net.minecraft.util.ResourceLocation;
 
-public class BasicHorseArmorItem extends HorseArmorItem implements IURegistryType {
+public class BasicHorseArmorItem extends DyeableHorseArmorItem implements IColoredArmor, IURegistryType {
 	
 	protected final String name;
 	
 	public BasicHorseArmorItem(String name, Rarity rarity, int armorPoints) {
-		super(armorPoints, new ResourceLocation(UsefulResourcesMod.MODID, "textures/entity/horse/armor/" + name + ".png"), new Properties().group(UsefulResourcesItemGroups.GROUP));
+		super(armorPoints, new ResourceLocation(UsefulResourcesMod.MODID, "textures/entity/horse/armor/horse_armor.png"), new Properties().group(UsefulResourcesItemGroups.GROUP));
 		this.name = name + "_horse_armor";
 	}
 	
