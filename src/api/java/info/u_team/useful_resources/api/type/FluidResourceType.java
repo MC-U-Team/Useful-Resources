efcc4799579ef45a47500e9cebd8eebda4e9aed9
HyCraftHD <hycrafthd@live.de>
2020-03-05 02:21:59 +0100

    Add fluid resource type

--- src/api/java/info/u_team/useful_resources/api/type/FluidResourceType.java
+++ src/api/java/info/u_team/useful_resources/api/type/FluidResourceType.java
@@ -2,24 +2,39 @@ package info.u_team.useful_resources.api.type;
 
 import info.u_team.useful_resources.api.resource.IResource;
 import net.minecraft.fluid.Fluid;
-import net.minecraft.tags.Tag;
+import net.minecraft.tags.*;
+import net.minecraft.util.ResourceLocation;
 
 public enum FluidResourceType implements IResourceType<Fluid> {
-	;
 	
-	@Override
+	MOLTEN_("molten", "molten"),
+	MOLTEN_FLOWING("molten_flowing", "molten");
+	
+	private final String name;
+	
+	private final String tagName;
+	
+	private FluidResourceType(String name) {
+		this(name, name + "s");
+	}
+	
+	private FluidResourceType(String name, String tagName) {
+		this.name = name;
+		this.tagName = tagName;
+	}
+	
 	public String getName() {
-		return null;
+		return name;
 	}
 	
 	@Override
 	public Tag<Fluid> getUnifyTag() {
-		return null;
+		return new FluidTags.Wrapper(new ResourceLocation("forge", tagName));
 	}
 	
 	@Override
 	public Tag<Fluid> getTag(IResource resource) {
-		return null;
+		return new FluidTags.Wrapper(new ResourceLocation("forge", tagName + "/" + resource.getName()));
 	}
 	
 }
