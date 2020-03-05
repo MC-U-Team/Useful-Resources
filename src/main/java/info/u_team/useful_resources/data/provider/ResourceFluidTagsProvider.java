d666ed45369189c0b78fbebec397303abf56c5b4
HyCraftHD <hycrafthd@live.de>
2020-03-05 18:09:20 +0100

    Add fluid tags provider

---
+++ src/main/java/info/u_team/useful_resources/data/provider/ResourceFluidTagsProvider.java
@@ -0,0 +1,29 @@
+package info.u_team.useful_resources.data.provider;
+
+import info.u_team.move_to_u_team_core.CommonFluidTagsProvider;
+import info.u_team.u_team_core.data.GenerationData;
+import info.u_team.useful_resources.api.ResourceRegistry;
+import net.minecraft.fluid.Fluid;
+import net.minecraft.tags.Tag;
+
+public class ResourceFluidTagsProvider extends CommonFluidTagsProvider {
+	
+	public ResourceFluidTagsProvider(GenerationData data) {
+		super(data);
+	}
+	
+	@Override
+	protected void registerTags() {
+		ResourceRegistry.getResources().forEach(resource -> {
+			resource.getFluids().forEach((type, fluid) -> {
+				if (type.hasTag()) {
+					final Tag<Fluid> tag = type.getTag(resource);
+					getBuilder(tag).add(fluid);
+					if (type.hasUnifyTag()) {
+						getBuilder(type.getUnifyTag()).add(tag);
+					}
+				}
+			});
+		});
+	}
+}
