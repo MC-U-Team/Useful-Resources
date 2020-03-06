8d353cedcbabcdb1bbb1e5f5018bd689b142dadd
HyCraftHD <hycrafthd@live.de>
2020-03-06 06:08:54 +0100

    Add common armor and tool tags

--- src/main/java/info/u_team/useful_resources/data/provider/ResourceItemTagsProvider.java
+++ src/main/java/info/u_team/useful_resources/data/provider/ResourceItemTagsProvider.java
@@ -3,6 +3,7 @@ package info.u_team.useful_resources.data.provider;
 import info.u_team.u_team_core.data.*;
 import info.u_team.u_team_core.util.TagUtil;
 import info.u_team.useful_resources.api.ResourceRegistry;
+import info.u_team.useful_resources.api.type.ItemResourceType;
 import net.minecraft.item.Item;
 import net.minecraft.tags.Tag;
 
@@ -36,5 +37,8 @@ public class ResourceItemTagsProvider extends CommonItemTagsProvider {
 				}
 			});
 		});
+		
+		getBuilder(TagUtil.createItemTag("forge", "tools")).add(ItemResourceType.AXE.getUnifyTag(), ItemResourceType.HOE.getUnifyTag(), ItemResourceType.PICKAXE.getUnifyTag(), ItemResourceType.SHOVEL.getUnifyTag(), ItemResourceType.SWORD.getUnifyTag());
+		getBuilder(TagUtil.createItemTag("forge", "armors")).add(ItemResourceType.HELMET.getUnifyTag(), ItemResourceType.CHESTPLATE.getUnifyTag(), ItemResourceType.LEGGINGS.getUnifyTag(), ItemResourceType.BOOTS.getUnifyTag());
 	}
 }
