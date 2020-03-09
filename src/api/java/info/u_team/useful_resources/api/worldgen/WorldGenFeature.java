f1856a973c034c6016132f0bca0a81dc3d6f217e
HyCraftHD <hycrafthd@live.de>
2020-03-09 16:15:08 +0100

    Implement decoration feature and add to serialize and deserialize

--- src/api/java/info/u_team/useful_resources/api/worldgen/WorldGenFeature.java
+++ src/api/java/info/u_team/useful_resources/api/worldgen/WorldGenFeature.java
@@ -6,6 +6,7 @@ import com.google.common.collect.ImmutableMap;
 import com.mojang.datafixers.Dynamic;
 import com.mojang.datafixers.types.DynamicOps;
 
+import net.minecraft.world.gen.GenerationStage.Decoration;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 
 public class WorldGenFeature implements IWorldGenFeature {
@@ -13,11 +14,14 @@ public class WorldGenFeature implements IWorldGenFeature {
 	private final CategoryTypeList categories;
 	private final BiomeTypeList biomes;
 	
+	private final Decoration decoration;
+	
 	private final ConfiguredFeature<?, ?> feature;
 	
-	public WorldGenFeature(CategoryTypeList categories, BiomeTypeList biomes, ConfiguredFeature<?, ?> feature) {
+	public WorldGenFeature(CategoryTypeList categories, BiomeTypeList biomes, Decoration decoration, ConfiguredFeature<?, ?> feature) {
 		this.categories = Objects.requireNonNull(categories);
 		this.biomes = Objects.requireNonNull(biomes);
+		this.decoration = Objects.requireNonNull(decoration);
 		this.feature = Objects.requireNonNull(feature);
 	}
 	
@@ -32,19 +36,25 @@ public class WorldGenFeature implements IWorldGenFeature {
 	}
 	
 	@Override
+	public Decoration getDecoration() {
+		return decoration;
+	}
+	
+	@Override
 	public ConfiguredFeature<?, ?> getFeature() {
 		return feature;
 	}
 	
 	public <T> Dynamic<T> serialize(DynamicOps<T> ops) {
-		return new Dynamic<>(ops, ops.createMap(ImmutableMap.of(ops.createString("categories"), categories.serialize(ops).getValue(), ops.createString("biomes"), biomes.serialize(ops).getValue(), ops.createString("feature"), feature.serialize(ops).getValue())));
+		return new Dynamic<>(ops, ops.createMap(ImmutableMap.of(ops.createString("categories"), categories.serialize(ops).getValue(), ops.createString("biomes"), biomes.serialize(ops).getValue(), ops.createString("decoration"), ops.createString(decoration.getName()), ops.createString("feature"), feature.serialize(ops).getValue())));
 	}
 	
 	public static <T> WorldGenFeature deserialize(Dynamic<T> ops) {
 		final CategoryTypeList categories = CategoryTypeList.deserialize(ops.get("categories").orElseEmptyMap());
 		final BiomeTypeList biomes = BiomeTypeList.deserialize(ops.get("biomes").orElseEmptyMap());
-		final ConfiguredFeature<?, ?> feature = ConfiguredFeature.deserialize(ops.get("biomes").orElseEmptyMap());
-		return new WorldGenFeature(categories, biomes, feature);
+		final Decoration decoration = Decoration.BY_NAME.getOrDefault(ops.get("decoration").asString(""), Decoration.UNDERGROUND_DECORATION);
+		final ConfiguredFeature<?, ?> feature = ConfiguredFeature.deserialize(ops.get("feature").orElseEmptyMap());
+		return new WorldGenFeature(categories, biomes, decoration, feature);
 	}
 	
 }
