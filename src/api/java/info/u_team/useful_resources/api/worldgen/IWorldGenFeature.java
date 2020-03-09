deddaa5a5d151401fd72657d11e7e6a4d679d1ad
HyCraftHD <hycrafthd@live.de>
2020-03-09 04:12:47 +0100

    Add basic IWorldGenFeature interface

---
+++ src/api/java/info/u_team/useful_resources/api/worldgen/IWorldGenFeature.java
@@ -0,0 +1,15 @@
+package info.u_team.useful_resources.api.worldgen;
+
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.Biome.Category;
+import net.minecraft.world.gen.feature.*;
+
+public interface IWorldGenFeature<C extends IFeatureConfig> {
+	
+	Feature<C> getFeature();
+	
+	TypeList<Category> getCategories();
+	
+	TypeList<Biome> getBiomes();
+	
+}
