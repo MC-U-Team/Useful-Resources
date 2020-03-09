15a3f1a634cc4d2fe4be2d64d8d7e11ef9fd7f6c
HyCraftHD <hycrafthd@live.de>
2020-03-09 04:11:30 +0100

    Add typed list

---
+++ src/api/java/info/u_team/useful_resources/api/worldgen/TypeList.java
@@ -0,0 +1,33 @@
+package info.u_team.useful_resources.api.worldgen;
+
+import java.util.List;
+
+public class TypeList<E> {
+	
+	private final ListType type;
+	private final List<E> list;
+	
+	public TypeList(ListType type, List<E> list) {
+		this.type = type;
+		this.list = list;
+	}
+	
+	public ListType getType() {
+		return type;
+	}
+	
+	public List<E> getList() {
+		return list;
+	}
+	
+	/**
+	 * Returns true if the element is accepted and not blacklisted or whitelisted
+	 * 
+	 * @param element
+	 * @return true if element is accepted
+	 */
+	public boolean testWithType(E element) {
+		return !(type == ListType.BLACKLIST && list.contains(element) || type == ListType.WHITELIST && !list.contains(element));
+	}
+	
+}
