341b9e22938b60e6f464bd82ba668d28e181e362
HyCraftHD <hycrafthd@live.de>
2020-03-09 04:20:54 +0100

    Update abstract world gen feature

---
+++ src/api/java/info/u_team/useful_resources/api/worldgen/AbstractWorldGenFeature.java
@@ -0,0 +1,36 @@
+package info.u_team.useful_resources.api.worldgen;
+
+import info.u_team.useful_resources.api.list.TypeList;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.Biome.Category;
+import net.minecraft.world.gen.feature.ConfiguredFeature;
+
+public class AbstractWorldGenFeature implements IWorldGenFeature {
+	
+	private final ConfiguredFeature<?, ?> feature;
+	
+	private final TypeList<Category> categories;
+	private final TypeList<Biome> biomes;
+	
+	public AbstractWorldGenFeature(ConfiguredFeature<?, ?> feature, TypeList<Category> categories, TypeList<Biome> biomes) {
+		this.feature = feature;
+		this.categories = categories;
+		this.biomes = biomes;
+	}
+	
+	@Override
+	public ConfiguredFeature<?, ?> getFeature() {
+		return feature;
+	}
+	
+	@Override
+	public TypeList<Category> getCategories() {
+		return categories;
+	}
+	
+	@Override
+	public TypeList<Biome> getBiomes() {
+		return biomes;
+	}
+	
+}
