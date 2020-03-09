939df761ff45d32af9226a161a6107aab2dfbc35
HyCraftHD <hycrafthd@live.de>
2020-03-09 14:37:31 +0100

    Update biome list type to filter wrong values

---
+++ src/api/java/info/u_team/useful_resources/api/worldgen/BiomeTypeList.java
@@ -0,0 +1,29 @@
+package info.u_team.useful_resources.api.worldgen;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.google.common.collect.ImmutableMap;
+import com.mojang.datafixers.Dynamic;
+import com.mojang.datafixers.types.DynamicOps;
+
+import info.u_team.useful_resources.api.list.*;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.biome.Biome;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class BiomeTypeList extends TypeList<Biome> {
+	
+	public BiomeTypeList(ListType type, List<Biome> list) {
+		super(type, list);
+	}
+	
+	public <T> Dynamic<T> serialize(DynamicOps<T> ops) {
+		return new Dynamic<>(ops, ops.createMap(ImmutableMap.of(ops.createString("type"), ops.createString(getType().getName()), ops.createString("entries"), ops.createList(getList().stream().map(entry -> ops.createString(entry.getRegistryName().toString()))))));
+	}
+	
+	public static <T> BiomeTypeList deserialize(Dynamic<T> ops) {
+		return new BiomeTypeList(ListType.byName(ops.get("type").asString("")), ops.get("entries").asStream().map(entry -> ResourceLocation.tryCreate(entry.asString(""))).filter(ForgeRegistries.BIOMES::containsKey).map(ForgeRegistries.BIOMES::getValue).collect(Collectors.toList()));
+	}
+	
+}
