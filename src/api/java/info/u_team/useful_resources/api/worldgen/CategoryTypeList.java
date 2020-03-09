ad03d16bd3322d62816b1445df1308848277113a
HyCraftHD <hycrafthd@live.de>
2020-03-09 04:58:36 +0100

    Add category type with serialize methods

---
+++ src/api/java/info/u_team/useful_resources/api/worldgen/CategoryTypeList.java
@@ -0,0 +1,26 @@
+package info.u_team.useful_resources.api.worldgen;
+
+import java.util.List;
+
+import com.google.common.collect.ImmutableMap;
+import com.mojang.datafixers.Dynamic;
+import com.mojang.datafixers.types.DynamicOps;
+
+import info.u_team.useful_resources.api.list.*;
+import net.minecraft.world.biome.Biome.Category;
+
+public class CategoryTypeList extends TypeList<Category> {
+	
+	public CategoryTypeList(ListType type, List<Category> list) {
+		super(type, list);
+	}
+	
+	public <T> Dynamic<T> serialize(DynamicOps<T> ops) {
+		return new Dynamic<>(ops, ops.createMap(ImmutableMap.of(ops.createString("type"), ops.createString(getType().getName()), ops.createString("entries"), ops.createList(getList().stream().map(entry -> ops.createString(entry.getName()))))));
+	}
+	
+	public static <T> CategoryTypeList deserialize(Dynamic<T> ops) {
+		return new CategoryTypeList(ListType.byName(ops.get("type").asString("")), ops.get("entries").asList(entry -> Category.BY_NAME.getOrDefault(entry.asString(""), Category.NONE)));
+	}
+	
+}
