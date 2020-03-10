9a5919ba9a28cfe8388c87caddd7333359d95765
HyCraftHD <hycrafthd@live.de>
2020-03-10 16:36:31 +0100

    Add world gen feature provider

---
+++ src/main/java/info/u_team/useful_resources/data/provider/ResourceWorldGenFeatureProvider.java
@@ -0,0 +1,16 @@
+package info.u_team.useful_resources.data.provider;
+
+import info.u_team.u_team_core.data.GenerationData;
+import info.u_team.useful_resources.data.provider.common.CommonWorldGenFeatureProvider;
+
+public class ResourceWorldGenFeatureProvider extends CommonWorldGenFeatureProvider {
+	
+	public ResourceWorldGenFeatureProvider(GenerationData data) {
+		super(data);
+	}
+	
+	@Override
+	protected void addFeatures() {
+	}
+	
+}
