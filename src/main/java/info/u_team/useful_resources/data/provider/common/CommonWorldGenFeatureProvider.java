131ff5694d4eddd10958688a5b99da45e95363fc
HyCraftHD <hycrafthd@live.de>
2020-03-10 15:56:57 +0100

    Add common feature provider to generate

---
+++ src/main/java/info/u_team/useful_resources/data/provider/common/CommonWorldGenFeatureProvider.java
@@ -0,0 +1,40 @@
+package info.u_team.useful_resources.data.provider.common;
+
+import java.io.IOException;
+import java.util.*;
+
+import com.mojang.datafixers.types.JsonOps;
+
+import info.u_team.u_team_core.data.*;
+import info.u_team.useful_resources.api.worldgen.WorldGenFeature;
+import net.minecraft.data.DirectoryCache;
+
+public abstract class CommonWorldGenFeatureProvider extends CommonProvider {
+	
+	private final Map<String, WorldGenFeature> data;
+	
+	public CommonWorldGenFeatureProvider(GenerationData data) {
+		super(data);
+		this.data = new HashMap<>();
+	}
+	
+	@Override
+	public void act(DirectoryCache cache) throws IOException {
+		addFeatures();
+		data.forEach((path, feature) -> {
+			try {
+				write(cache, GSON.toJson(feature.serialize(JsonOps.INSTANCE).getValue()), resolveModData().resolve("useful_resources").resolve("worldgen_feature").resolve(path + ".json"));
+			} catch (final IOException ex) {
+				LOGGER.error(marker, "Could not write data.", ex);
+			}
+		});
+	}
+	
+	protected abstract void addFeatures();
+	
+	@Override
+	public String getName() {
+		return "Worldgen-Feature";
+	}
+	
+}
