20acb74dc8de0a49cc4605773250b470600693f7
HyCraftHD <hycrafthd@live.de>
2020-03-10 18:13:00 +0100

    Add special provider

---
+++ src/main/java/info/u_team/useful_resources/data/provider/ResourceSpecialProvider.java
@@ -0,0 +1,26 @@
+package info.u_team.useful_resources.data.provider;
+
+import java.io.IOException;
+
+import com.google.gson.JsonObject;
+
+import info.u_team.u_team_core.data.*;
+import net.minecraft.data.DirectoryCache;
+
+public class ResourceSpecialProvider extends CommonProvider {
+	
+	public ResourceSpecialProvider(GenerationData data) {
+		super(data);
+	}
+	
+	@Override
+	public void act(DirectoryCache cache) throws IOException {
+		write(cache, new JsonObject(), resolveModAssets().resolve("particles").resolve("colored_overlay_block.json"));
+	}
+	
+	@Override
+	public String getName() {
+		return "Special-Provider";
+	}
+	
+}
