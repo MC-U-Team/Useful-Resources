ec2ed9b3b2f311eba3bc2e4bd3f7c5022c5602ef
HyCraftHD <hycrafthd@live.de>
2020-03-10 17:15:16 +0100

    Add generator

--- src/main/java/info/u_team/useful_resources/data/provider/ResourceWorldGenFeatureProvider.java
+++ src/main/java/info/u_team/useful_resources/data/provider/ResourceWorldGenFeatureProvider.java
@@ -1,6 +1,7 @@
 package info.u_team.useful_resources.data.provider;
 
 import info.u_team.u_team_core.data.GenerationData;
+import info.u_team.useful_resources.api.ResourceRegistry;
 import info.u_team.useful_resources.data.provider.common.CommonWorldGenFeatureProvider;
 
 public class ResourceWorldGenFeatureProvider extends CommonWorldGenFeatureProvider {
@@ -11,6 +12,11 @@ public class ResourceWorldGenFeatureProvider extends CommonWorldGenFeatureProvid
 	
 	@Override
 	protected void addFeatures() {
+		ResourceRegistry.getResources().forEach(resource -> {
+			resource.getDataGeneratorConfigurator().getWorldGeneration().forEach((name, worldGenFeature) -> {
+				addFeature(resource.getName() + "/" + name, worldGenFeature);
+			});
+		});
 	}
 	
 }
