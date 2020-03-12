941a1a8acb67a9bbce89b8f1c2d328e28090bc85
HyCraftHD <hycrafthd@live.de>
2020-03-12 02:29:03 +0100

    Add some basic recipes

--- src/main/java/info/u_team/useful_resources/data/provider/ResourceRecipesProvider.java
+++ src/main/java/info/u_team/useful_resources/data/provider/ResourceRecipesProvider.java
@@ -2,8 +2,9 @@ package info.u_team.useful_resources.data.provider;
 
 import static info.u_team.useful_resources.api.type.BlockResourceType.*;
 import static info.u_team.useful_resources.api.type.ItemResourceType.*;
-
 import static net.minecraft.data.CookingRecipeBuilder.*;
+import static net.minecraft.data.ShapedRecipeBuilder.shapedRecipe;
+import static net.minecraft.data.ShapelessRecipeBuilder.shapelessRecipe;
 
 import java.util.Map;
 import java.util.function.Consumer;
@@ -13,8 +14,9 @@ import info.u_team.u_team_core.util.TagUtil;
 import info.u_team.useful_resources.api.ResourceRegistry;
 import info.u_team.useful_resources.api.resource.IResource;
 import info.u_team.useful_resources.api.type.*;
+import net.minecraft.advancements.criterion.*;
 import net.minecraft.block.Block;
-import net.minecraft.data.*;
+import net.minecraft.data.IFinishedRecipe;
 import net.minecraft.item.Item;
 import net.minecraft.tags.Tag;
 import net.minecraft.util.ResourceLocation;
@@ -72,6 +74,52 @@ public class ResourceRecipesProvider extends CommonRecipesProvider {
 						.addCriterion("has_dust", hasItem(dustTag)) //
 						.build(consumer, createLocation(resource, "blasting/ingot_from_dust"));
 			}
+			
+			// INGOT <-> BLOCK
+			if (items.containsKey(INGOT) && blocks.containsKey(BLOCK)) {
+				// INGOT -> BLOCK
+				final Tag<Item> ingotTag = INGOT.getTag(resource);
+				final Block blockBlock = blocks.get(BLOCK);
+				shapedRecipe(blockBlock) //
+						.key('#', ingotTag) //
+						.patternLine("###") //
+						.patternLine("###") //
+						.patternLine("###") //
+						.addCriterion("has_ingot", hasItem(ingotTag)) //
+						.build(consumer, createLocation(resource, "crafting/block_from_ingot"));
+				
+				// BLOCK -> INGOT
+				final Tag<Item> blockTag = getItemTag(BLOCK, resource);
+				final Item ingotItem = items.get(INGOT);
+				
+				shapelessRecipe(ingotItem, 9) //
+						.addIngredient(blockTag) //
+						.addCriterion("has_block", hasItem(blockTag)) //
+						.build(consumer, createLocation(resource, "crafting/ingot_from_block"));
+			}
+			
+			// NUGGET <-> INGOT
+			if (items.containsKey(NUGGET) && items.containsKey(INGOT)) {
+				// NUGGET -> INGOT
+				final Tag<Item> nuggetTag = NUGGET.getTag(resource);
+				final Item ingotItem = items.get(INGOT);
+				shapedRecipe(ingotItem) //
+						.key('#', nuggetTag) //
+						.patternLine("###") //
+						.patternLine("###") //
+						.patternLine("###") //
+						.addCriterion("has_nugget", hasItem(nuggetTag)) //
+						.build(consumer, createLocation(resource, "crafting/ingot_from_nugget"));
+				
+				// INGOT -> NUGGET
+				final Tag<Item> ingotTag = INGOT.getTag(resource);
+				final Item nuggetItem = items.get(NUGGET);
+				
+				shapelessRecipe(nuggetItem, 9) //
+						.addIngredient(ingotTag) //
+						.addCriterion("has_ingot", hasItem(ingotTag)) //
+						.build(consumer, createLocation(resource, "crafting/nugget_from_ingot"));
+			}
 		});
 	}
 	
@@ -81,6 +129,10 @@ public class ResourceRecipesProvider extends CommonRecipesProvider {
 	
 	private ResourceLocation createLocation(IResource resource, String name) {
 		return new ResourceLocation(modid, resource.getName() + "/" + name);
+	}
+	
+	protected InventoryChangeTrigger.Instance hasItem(Tag<Item> tag) {
+		return hasItem(ItemPredicate.Builder.create().tag(tag).build());
 	}
 	
 }
