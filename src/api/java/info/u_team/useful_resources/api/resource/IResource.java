9626d169a00d8832521064f5024d1f2ea672dde6
HyCraftHD <hycrafthd@live.de>
2020-03-24 18:03:15 +0100

    Add method to iterate over resources and registry resources

--- src/api/java/info/u_team/useful_resources/api/resource/IResource.java
+++ src/api/java/info/u_team/useful_resources/api/resource/IResource.java
@@ -1,6 +1,8 @@
 package info.u_team.useful_resources.api.resource;
 
 import java.util.*;
+import java.util.Map.Entry;
+import java.util.function.BiConsumer;
 
 import info.u_team.useful_resources.api.feature.IResourceFeatureBuilder;
 import info.u_team.useful_resources.api.resource.data.IDataGeneratorConfigurator;
@@ -8,6 +10,7 @@ import info.u_team.useful_resources.api.type.*;
 import net.minecraft.block.Block;
 import net.minecraft.fluid.Fluid;
 import net.minecraft.item.Item;
+import net.minecraftforge.registries.IForgeRegistryEntry;
 
 public interface IResource {
 	
@@ -28,6 +31,33 @@ public interface IResource {
 	List<Fluid> getRegistryFluids();
 	
 	List<Item> getRegistryItems();
+	
+	default void iterateRegistryBlocks(BiConsumer<BlockResourceType, Block> consumer) {
+		iterateRegistry(getRegistryBlocks(), getBlocks(), consumer);
+	}
+	
+	default void iterateRegistryFluids(BiConsumer<FluidResourceType, Fluid> consumer) {
+		iterateRegistry(getRegistryFluids(), getFluids(), consumer);
+	}
+	
+	default void iterateRegistryItems(BiConsumer<ItemResourceType, Item> consumer) {
+		iterateRegistry(getRegistryItems(), getItems(), consumer);
+	}
+	
+	default <E extends IResourceType<?>, T extends IForgeRegistryEntry<T>> void iterateRegistry(List<T> registryList, Map<E, T> map, BiConsumer<E, T> consumer) {
+		registryList.stream().forEach(block -> {
+			consumer.accept( //
+					map //
+							.entrySet() //
+							.stream() //
+							.limit(1) //
+							.filter(entry -> block.equals(entry.getValue())) //
+							.map(Entry::getKey) //
+							.findAny() //
+							.get(), //
+					block);
+		});
+	}
 	
 	IDataGeneratorConfigurator getDataGeneratorConfigurator();
 	
