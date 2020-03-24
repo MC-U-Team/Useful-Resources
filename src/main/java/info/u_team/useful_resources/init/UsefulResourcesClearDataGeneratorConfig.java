7a59f33fe0a882988c91fab4e5ecc5a77270bb1b
HyCraftHD <hycrafthd@live.de>
2020-03-24 14:30:38 +0100

    Clear data generator

---
+++ src/main/java/info/u_team/useful_resources/init/UsefulResourcesClearDataGeneratorConfig.java
@@ -0,0 +1,19 @@
+package info.u_team.useful_resources.init;
+
+import info.u_team.useful_resources.UsefulResourcesMod;
+import info.u_team.useful_resources.api.ResourceRegistry;
+import info.u_team.useful_resources.api.resource.IResource;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+
+@EventBusSubscriber(modid = UsefulResourcesMod.MODID, bus = Bus.MOD)
+public class UsefulResourcesClearDataGeneratorConfig {
+	
+	@SubscribeEvent
+	public static void setup(FMLCommonSetupEvent event) {
+		ResourceRegistry.getResources().forEach(IResource::clearDataGeneratorConfig);
+	}
+	
+}
