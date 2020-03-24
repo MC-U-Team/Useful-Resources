ef69264554a85f7d6495099343af69048e8bf10f
HyCraftHD <hycrafthd@live.de>
2020-03-24 14:42:11 +0100

    Add basic vanilla ingot resource for extendend iron and gold

---
+++ src/api/java/info/u_team/useful_resources/api/resource/special/VanillaIngotResource.java
@@ -0,0 +1,24 @@
+package info.u_team.useful_resources.api.resource.special;
+
+import static info.u_team.useful_resources.api.resource.CommonResourceBuilder.*;
+import static info.u_team.useful_resources.api.type.BlockResourceType.NETHER_ORE;
+import static info.u_team.useful_resources.api.type.ItemResourceType.*;
+
+import info.u_team.useful_resources.api.type.ItemResourceType;
+import net.minecraft.item.Rarity;
+
+public class VanillaIngotResource extends BasicResource<VanillaIngotResource> {
+	
+	public VanillaIngotResource(String name, int color, int harvestLevel, float baseHardness) {
+		super(name, color, ItemResourceType.INGOT, Rarity.COMMON);
+		addFeature(createOre(NETHER_ORE, Rarity.COMMON, harvestLevel, baseHardness * 0.75F, baseHardness * 1.25F));
+		
+		addFeature(createMoltenFluid(0xFF000000 + color));
+		
+		addFeature(createBasicItem(DUST, Rarity.COMMON));
+		addFeature(createBasicItem(PLATE, Rarity.COMMON));
+		addFeature(createBasicItem(DENSE_PLATE, Rarity.COMMON));
+		addFeature(createBasicItem(GEAR, Rarity.COMMON));
+		addFeature(createBasicItem(ROD, Rarity.COMMON));
+	}
+}
