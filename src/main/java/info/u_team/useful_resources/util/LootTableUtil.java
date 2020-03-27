2a60c4e318f23e6264cdb71f86f42d3d38b7e989
HyCraftHD <hycrafthd@live.de>
2020-03-27 14:13:49 +0100

    Add method for count

--- src/main/java/info/u_team/useful_resources/util/LootTableUtil.java
+++ src/main/java/info/u_team/useful_resources/util/LootTableUtil.java
@@ -1,14 +1,33 @@
 package info.u_team.useful_resources.util;
 
 import info.u_team.useful_resources.data.provider.ResourceLootTableProvider;
+import net.minecraft.advancements.criterion.*;
 import net.minecraft.block.Block;
+import net.minecraft.enchantment.Enchantments;
 import net.minecraft.util.IItemProvider;
-import net.minecraft.world.storage.loot.LootTable;
+import net.minecraft.world.storage.loot.*;
+import net.minecraft.world.storage.loot.conditions.MatchTool;
+import net.minecraft.world.storage.loot.functions.*;
 
 public class LootTableUtil {
 	
 	public static LootTable createFortuneBlockLootTable(Block block, IItemProvider item) {
 		return ResourceLootTableProvider.addFortuneBlockLootTable(block, item);
+	}
+	
+	public static LootTable createFortuneBlockLootTableWithCount(Block block, IItemProvider item, float min, float max) {
+		return LootTable.builder() //
+				.setParameterSet(LootParameterSets.BLOCK) //
+				.addLootPool(LootPool.builder() //
+						.rolls(ConstantRange.of(1)) //
+						.addEntry(ItemLootEntry.builder(block) //
+								.acceptCondition(MatchTool.builder(ItemPredicate.Builder.create() //
+										.enchantment(new EnchantmentPredicate(Enchantments.SILK_TOUCH, MinMaxBounds.IntBound.atLeast(1)))) //
+								).alternatively(ItemLootEntry.builder(item) //
+										.acceptFunction(SetCount.builder(RandomValueRange.of(min, max))) //
+										.acceptFunction(ApplyBonus.oreDrops(Enchantments.FORTUNE)) //
+										.acceptFunction(ExplosionDecay.builder()))))
+				.build();
 	}
 	
 }
