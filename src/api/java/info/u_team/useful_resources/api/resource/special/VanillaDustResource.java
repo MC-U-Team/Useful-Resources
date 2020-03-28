2229fdf2f7754ab64a3048d1153fe92c77b2ad5a
HyCraftHD <hycrafthd@live.de>
2020-03-28 15:00:28 +0100

    Set dust value in the vanilla dust resource

--- src/api/java/info/u_team/useful_resources/api/resource/special/VanillaDustResource.java
+++ src/api/java/info/u_team/useful_resources/api/resource/special/VanillaDustResource.java
@@ -11,7 +11,7 @@ import net.minecraft.item.Rarity;
 public class VanillaDustResource extends BasicResource<VanillaDustResource> {
 	
 	public VanillaDustResource(String name, int color, int harvestLevel, float hardness, float resistance) {
-		super(name, color, ItemResourceType.DUST, Rarity.COMMON, ResourceType.INGOT);
+		super(name, color, ItemResourceType.DUST, Rarity.COMMON, ResourceType.DUST);
 		addFeature(createOre(NETHER_ORE, Rarity.COMMON, harvestLevel, hardness, resistance));
 		
 		addFeature(createMoltenFluid(0xFF000000 + color));
