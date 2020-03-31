1484d593a845629ea6df43a51e151dce6c2d183f
HyCraftHD <hycrafthd@live.de>
2020-03-31 02:05:33 +0200

    Also add method for blocks

--- src/api/java/info/u_team/useful_resources/api/stack/ItemStackCountProvider.java
+++ src/api/java/info/u_team/useful_resources/api/stack/ItemStackCountProvider.java
@@ -1,13 +1,17 @@
 package info.u_team.useful_resources.api.stack;
 
 import info.u_team.useful_resources.api.resource.IResource;
-import info.u_team.useful_resources.api.type.ItemResourceType;
+import info.u_team.useful_resources.api.type.*;
 import net.minecraft.item.*;
 import net.minecraft.util.IItemProvider;
 
 public class ItemStackCountProvider implements IItemCountProvider {
 	
 	private final ItemStack stack;
+	
+	public ItemStackCountProvider(IResource resource, BlockResourceType type, int count) {
+		this(resource.getBlocks().get(type), count);
+	}
 	
 	public ItemStackCountProvider(IResource resource, ItemResourceType type, int count) {
 		this(resource.getItems().get(type), count);
