9dbaca45175c57ca434ce7cdd3c471fe935e1c6a
HyCraftHD <hycrafthd@live.de>
2020-03-31 19:42:30 +0200

    Add more collectors

---
+++ src/main/java/info/u_team/useful_resources/util/MoreCollectors.java
@@ -0,0 +1,15 @@
+package info.u_team.useful_resources.util;
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.*;
+
+public class MoreCollectors {
+	
+	public static <T, K, U> Collector<T, ?, Map<K, U>> toLinkedMap(Function<? super T, ? extends K> keyMapper, Function<? super T, ? extends U> valueMapper) {
+		return Collectors.toMap(keyMapper, valueMapper, (u, v) -> {
+			throw new IllegalStateException(String.format("Duplicate key %s", u));
+		}, LinkedHashMap::new);
+	}
+	
+}
