45b533ce84b77acae3e23ed5b00f0a4eb2ecf3b9
HyCraftHD <hycrafthd@live.de>
2020-03-31 16:45:18 +0200

    Rename the crushed nether ore type

--- src/api/java/info/u_team/useful_resources/api/type/ItemResourceType.java
+++ src/api/java/info/u_team/useful_resources/api/type/ItemResourceType.java
@@ -10,7 +10,7 @@ import net.minecraft.util.ResourceLocation;
 public enum ItemResourceType implements CacheResourceType<Item> {
 	
 	CRUSHED_ORE("crushed_ore"),
-	NETHER_CRUSHED_ORE("nether_crushed_ore"),
+	CRUSHED_NETHER_ORE("crushed_nether_ore"),
 	PURE_CRUSHED_ORE("pure_crushed_ore"),
 	
 	INGOT("ingot"),
