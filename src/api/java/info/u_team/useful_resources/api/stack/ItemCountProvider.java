3c8eef6b9a0bac6c86b2a65f2362c889961f5954
HyCraftHD <hycrafthd@live.de>
2020-03-31 02:02:11 +0200

    Implement getItemStack method

---
+++ src/api/java/info/u_team/useful_resources/api/stack/ItemCountProvider.java
@@ -0,0 +1,28 @@
+package info.u_team.useful_resources.api.stack;
+
+import net.minecraft.item.*;
+
+public class ItemCountProvider implements IItemCountProvider {
+	
+	private final ItemStack stack;
+	
+	public ItemCountProvider(ItemStack stack) {
+		this.stack = stack;
+	}
+	
+	@Override
+	public Item asItem() {
+		return stack.getItem();
+	}
+	
+	@Override
+	public int getCount() {
+		return stack.getCount();
+	}
+	
+	@Override
+	public ItemStack getItemStack() {
+		return stack;
+	}
+	
+}
