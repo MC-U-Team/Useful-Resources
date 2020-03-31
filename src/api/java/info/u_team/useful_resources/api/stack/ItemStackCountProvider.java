0838c1951b68ea3ac50d88b73795c210bd999df6
HyCraftHD <hycrafthd@live.de>
2020-03-31 02:03:52 +0200

    Add new methods to the itemstack provider

--- src/api/java/info/u_team/useful_resources/api/stack/ItemStackCountProvider.java
+++ src/api/java/info/u_team/useful_resources/api/stack/ItemStackCountProvider.java
@@ -1,12 +1,17 @@
 package info.u_team.useful_resources.api.stack;
 
 import net.minecraft.item.*;
+import net.minecraft.util.IItemProvider;
 
-public class ItemCountProvider implements IItemCountProvider {
+public class ItemStackCountProvider implements IItemCountProvider {
 	
 	private final ItemStack stack;
 	
-	public ItemCountProvider(ItemStack stack) {
+	public ItemStackCountProvider(IItemProvider item, int count) {
+		this(new ItemStack(item, count));
+	}
+	
+	public ItemStackCountProvider(ItemStack stack) {
 		this.stack = stack;
 	}
 	
