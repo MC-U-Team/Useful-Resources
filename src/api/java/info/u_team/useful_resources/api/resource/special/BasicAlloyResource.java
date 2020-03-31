16826b6bab8a12cd09c6f5a43b555c38da59d6d2
HyCraftHD <hycrafthd@live.de>
2020-03-31 14:34:34 +0200

    Add method to return the right things

--- src/api/java/info/u_team/useful_resources/api/resource/special/BasicAlloyResource.java
+++ src/api/java/info/u_team/useful_resources/api/resource/special/BasicAlloyResource.java
@@ -6,23 +6,19 @@ import static info.u_team.useful_resources.api.type.ItemResourceType.*;
 
 import java.util.*;
 
-import com.google.common.base.Supplier;
-
-import info.u_team.useful_resources.api.resource.*;
+import info.u_team.useful_resources.api.resource.BasicResource;
 import info.u_team.useful_resources.api.resource.data.IDataGeneratorConfigurator.ResourceType;
+import info.u_team.useful_resources.api.stack.IItemCountProvider;
 import info.u_team.useful_resources.api.type.ItemResourceType;
 import net.minecraft.item.Rarity;
 
 public class BasicAlloyResource extends BasicResource<BasicAlloyResource> {
 	
-	private Map<IResource, Integer> ingredients;
-	
-	private Map<Supplier<IResource>, Integer> supplierIngredients;
+	private List<IItemCountProvider> ingredients;
 	
 	public BasicAlloyResource(String name, int color, Rarity rarity, int harvestLevel, float baseHardness) {
 		super(name, color, ItemResourceType.INGOT, rarity, ResourceType.INGOT);
-		ingredients = new HashMap<>();
-		supplierIngredients = new HashMap<>();
+		ingredients = new ArrayList<>();
 		
 		addFeature(createBasicBlock(BLOCK, rarity, harvestLevel, baseHardness * 2, baseHardness * 4));
 		
@@ -37,16 +33,12 @@ public class BasicAlloyResource extends BasicResource<BasicAlloyResource> {
 		addFeature(createBasicItem(ROD, rarity));
 	}
 	
-	public BasicAlloyResource addIngredient(Supplier<IResource> resource, int count) {
-		supplierIngredients.put(resource, count);
+	public BasicAlloyResource addIngredient(IItemCountProvider provider) {
+		ingredients.add(provider);
 		return this;
 	}
 	
-	public Map<IResource, Integer> getIngredients() {
-		if (ingredients.isEmpty() && !supplierIngredients.isEmpty()) {
-			supplierIngredients.forEach((supplier, count) -> ingredients.put(supplier.get(), count));
-			supplierIngredients.clear();
-		}
-		return Collections.unmodifiableMap(ingredients);
+	public List<IItemCountProvider> getIngredients() {
+		return Collections.unmodifiableList(ingredients);
 	}
 }
