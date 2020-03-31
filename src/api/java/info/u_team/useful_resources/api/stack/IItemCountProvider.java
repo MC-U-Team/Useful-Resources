8f24cddab9b671f39cf3d7691421bd19c4953e5f
HyCraftHD <hycrafthd@live.de>
2020-03-31 02:01:49 +0200

    Add getItemStack method

--- src/api/java/info/u_team/useful_resources/api/stack/IItemCountProvider.java
+++ src/api/java/info/u_team/useful_resources/api/stack/IItemCountProvider.java
@@ -1,9 +1,12 @@
 package info.u_team.useful_resources.api.stack;
 
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.IItemProvider;
 
 public interface IItemCountProvider extends IItemProvider {
 	
 	int getCount();
+	
+	ItemStack getItemStack();
 	
 }
