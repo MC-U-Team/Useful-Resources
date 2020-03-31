cbc9c615f44f70bbf6de6ce5cb009cba020059c5
HyCraftHD <hycrafthd@live.de>
2020-03-31 02:05:12 +0200

    Add method for the resource

--- src/api/java/info/u_team/useful_resources/api/stack/ItemStackCountProvider.java
+++ src/api/java/info/u_team/useful_resources/api/stack/ItemStackCountProvider.java
@@ -1,11 +1,17 @@
 package info.u_team.useful_resources.api.stack;
 
+import info.u_team.useful_resources.api.resource.IResource;
+import info.u_team.useful_resources.api.type.ItemResourceType;
 import net.minecraft.item.*;
 import net.minecraft.util.IItemProvider;
 
 public class ItemStackCountProvider implements IItemCountProvider {
 	
 	private final ItemStack stack;
+	
+	public ItemStackCountProvider(IResource resource, ItemResourceType type, int count) {
+		this(resource.getItems().get(type), count);
+	}
 	
 	public ItemStackCountProvider(IItemProvider item, int count) {
 		this(new ItemStack(item, count));
