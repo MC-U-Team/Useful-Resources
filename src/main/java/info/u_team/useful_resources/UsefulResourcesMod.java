5a46830812d8ddaa377f1bafac9b83aa22d43a2c
HyCraftHD <hycrafthd@live.de>
2020-04-01 13:24:07 +0200

    Check now if jar is signed

--- src/main/java/info/u_team/useful_resources/UsefulResourcesMod.java
+++ src/main/java/info/u_team/useful_resources/UsefulResourcesMod.java
@@ -1,5 +1,6 @@
 package info.u_team.useful_resources;
 
+import info.u_team.u_team_core.util.verify.JarSignVerifier;
 import info.u_team.useful_resources.config.CommonConfig;
 import info.u_team.useful_resources.resources.Resources;
 import net.minecraftforge.fml.ModLoadingContext;
@@ -12,6 +13,7 @@ public class UsefulResourcesMod {
 	public static final String MODID = "usefulresources";
 	
 	public UsefulResourcesMod() {
+		JarSignVerifier.checkSigned(MODID);
 		ModLoadingContext.get().registerConfig(Type.COMMON, CommonConfig.CONFIG);
 		Resources.register();
 	}
