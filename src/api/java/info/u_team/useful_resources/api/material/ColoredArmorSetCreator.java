f9d45f42099a7cd6bf43fad376cf267a76ae33e8
HyCraftHD <hycrafthd@live.de>
2020-06-30 19:40:53 +0200

    Fix armor set creator

--- src/api/java/info/u_team/useful_resources/api/material/ColoredArmorSetCreator.java
+++ src/api/java/info/u_team/useful_resources/api/material/ColoredArmorSetCreator.java
@@ -2,31 +2,30 @@ package info.u_team.useful_resources.api.material;
 
 import info.u_team.u_team_core.item.armor.*;
 import info.u_team.u_team_core.util.ItemProperties;
-import info.u_team.useful_resources.UsefulResourcesMod;
-import net.minecraft.entity.Entity;
-import net.minecraft.inventory.EquipmentSlotType;
+import info.u_team.u_team_core.util.registry.CommonDeferredRegister;
 import net.minecraft.item.*;
 import net.minecraft.item.Item.Properties;
+import net.minecraftforge.fml.RegistryObject;
 
 public class ColoredArmorSetCreator {
 	
-	public static ArmorSet create(String name, Properties properties, IArmorMaterial material) {
-		return create(name, null, properties, material);
+	public static ArmorSet create(CommonDeferredRegister<Item> register, String name, Properties properties, IArmorMaterial material) {
+		return create(register, name, null, properties, material);
 	}
 	
-	public static ArmorSet create(String name, ItemGroup group, Properties properties, IArmorMaterial material) {
-		return new ArmorSet(new ColoredModelHelmetItem(name, group, new ItemProperties(properties), material), new ColoredModelChestplateItem(name, group, new ItemProperties(properties), material), new ColoredModelLeggingsItem(name, group, new ItemProperties(properties), material), new ColoredModelBootsItem(name, group, new ItemProperties(properties), material));
+	public static ArmorSet create(CommonDeferredRegister<Item> register, String name, ItemGroup group, Properties properties, IArmorMaterial material) {
+		final RegistryObject<UHelmetItem> helmet = register.register(name + "_helmet", () -> new ColoredModelHelmetItem(name, group, new ItemProperties(properties), material));
+		final RegistryObject<UChestplateItem> chestplate = register.register(name + "_chestplate", () -> new ColoredModelChestplateItem(name, group, new ItemProperties(properties), material));
+		final RegistryObject<ULeggingsItem> leggings = register.register(name + "_leggings", () -> new ColoredModelLeggingsItem(name, group, new ItemProperties(properties), material));
+		final RegistryObject<UBootsItem> boots = register.register(name + "_boots", () -> new ColoredModelBootsItem(name, group, new ItemProperties(properties), material));
+		
+		return new ArmorSet(helmet, chestplate, leggings, boots);
 	}
 	
 	private static class ColoredModelHelmetItem extends UHelmetItem implements IColoredArmor {
 		
 		public ColoredModelHelmetItem(String name, ItemGroup group, Properties properties, IArmorMaterial material) {
 			super(name, group, properties, material);
-		}
-		
-		@Override
-		public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
-			return getArmorTextureString(slot, type);
 		}
 	}
 	
@@ -35,22 +34,12 @@ public class ColoredArmorSetCreator {
 		public ColoredModelChestplateItem(String name, ItemGroup group, Properties properties, IArmorMaterial material) {
 			super(name, group, properties, material);
 		}
-		
-		@Override
-		public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
-			return getArmorTextureString(slot, type);
-		}
 	}
 	
 	private static class ColoredModelLeggingsItem extends ULeggingsItem implements IColoredArmor {
 		
 		public ColoredModelLeggingsItem(String name, ItemGroup group, Properties properties, IArmorMaterial material) {
 			super(name, group, properties, material);
-		}
-		
-		@Override
-		public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
-			return getArmorTextureString(slot, type);
 		}
 	}
 	
@@ -59,14 +48,5 @@ public class ColoredArmorSetCreator {
 		public ColoredModelBootsItem(String name, ItemGroup group, Properties properties, IArmorMaterial material) {
 			super(name, group, properties, material);
 		}
-		
-		@Override
-		public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
-			return getArmorTextureString(slot, type);
-		}
-	}
-	
-	private static String getArmorTextureString(EquipmentSlotType slot, String type) {
-		return String.format(UsefulResourcesMod.MODID + ":textures/models/armor/armor_layer_%d%s.png", (slot == EquipmentSlotType.LEGS ? 2 : 1), type == null ? "" : String.format("_%s", type));
 	}
 }
