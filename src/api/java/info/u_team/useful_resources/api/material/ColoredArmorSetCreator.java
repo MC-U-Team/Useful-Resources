f93801ecb1419409d138198195f1d757e6c6f480
HyCraftHD <hycrafthd@live.de>
2020-07-01 16:18:08 +0200

    Fix the color of the armor

--- src/api/java/info/u_team/useful_resources/api/material/ColoredArmorSetCreator.java
+++ src/api/java/info/u_team/useful_resources/api/material/ColoredArmorSetCreator.java
@@ -14,10 +14,10 @@ public class ColoredArmorSetCreator {
 	}
 	
 	public static ArmorSet create(CommonDeferredRegister<Item> register, String name, ItemGroup group, Properties properties, IArmorMaterial material) {
-		final RegistryObject<UHelmetItem> helmet = register.register(name + "_helmet", () -> new ColoredModelHelmetItem(name, group, new ItemProperties(properties), material));
-		final RegistryObject<UChestplateItem> chestplate = register.register(name + "_chestplate", () -> new ColoredModelChestplateItem(name, group, new ItemProperties(properties), material));
-		final RegistryObject<ULeggingsItem> leggings = register.register(name + "_leggings", () -> new ColoredModelLeggingsItem(name, group, new ItemProperties(properties), material));
-		final RegistryObject<UBootsItem> boots = register.register(name + "_boots", () -> new ColoredModelBootsItem(name, group, new ItemProperties(properties), material));
+		final RegistryObject<UHelmetItem> helmet = register.register(name + "_helmet", () -> new ColoredModelHelmetItem("armor", group, new ItemProperties(properties), material));
+		final RegistryObject<UChestplateItem> chestplate = register.register(name + "_chestplate", () -> new ColoredModelChestplateItem("armor", group, new ItemProperties(properties), material));
+		final RegistryObject<ULeggingsItem> leggings = register.register(name + "_leggings", () -> new ColoredModelLeggingsItem("armor", group, new ItemProperties(properties), material));
+		final RegistryObject<UBootsItem> boots = register.register(name + "_boots", () -> new ColoredModelBootsItem("armor", group, new ItemProperties(properties), material));
 		
 		return new ArmorSet(helmet, chestplate, leggings, boots);
 	}
