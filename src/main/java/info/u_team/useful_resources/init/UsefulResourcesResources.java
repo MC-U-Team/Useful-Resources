6b234950776e5c839c517ffb2b23e8d4efcd834c
HyCraftHD <hycrafthd@live.de>
2020-07-01 15:36:38 +0200

    Register the resource now

--- src/main/java/info/u_team/useful_resources/init/UsefulResourcesResources.java
+++ src/main/java/info/u_team/useful_resources/init/UsefulResourcesResources.java
@@ -1,10 +1,12 @@
 package info.u_team.useful_resources.init;
 
+import info.u_team.useful_resources.api.resource.IResource;
 import net.minecraftforge.eventbus.api.IEventBus;
 
 public class UsefulResourcesResources {
 	
 	public static void register(IEventBus bus) {
+		IResource.DEFERRED_PROVIDER.register(bus);
 	}
 	
 }
