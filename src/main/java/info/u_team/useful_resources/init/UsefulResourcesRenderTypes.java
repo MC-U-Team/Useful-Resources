ad7c4980cdce747f6d158a6442f1dd3bd5dc19be
HyCraftHD <hycrafthd@live.de>
2020-07-01 15:48:52 +0200

    Fix render types

--- src/main/java/info/u_team/useful_resources/init/UsefulResourcesRenderTypes.java
+++ src/main/java/info/u_team/useful_resources/init/UsefulResourcesRenderTypes.java
@@ -2,6 +2,7 @@ package info.u_team.useful_resources.init;
 
 import info.u_team.useful_resources.UsefulResourcesMod;
 import info.u_team.useful_resources.api.ResourceRegistry;
+import info.u_team.useful_resources.api.registry.RegistryEntry;
 import info.u_team.useful_resources.block.OreBlock;
 import net.minecraft.client.renderer.*;
 import net.minecraftforge.api.distmarker.Dist;
@@ -19,7 +20,7 @@ public class UsefulResourcesRenderTypes {
 		final RenderType cutout = RenderType.getCutout();
 		
 		ResourceRegistry.getResources().stream() //
-				.flatMap(resource -> resource.getBlocks().values().stream()) //
+				.flatMap(resource -> resource.getBlocks().values().stream().map(RegistryEntry::get)) //
 				.filter(block -> block instanceof OreBlock) //
 				.map(block -> (OreBlock) block) //
 				.forEach(block -> RenderTypeLookup.setRenderLayer(block, cutout));
