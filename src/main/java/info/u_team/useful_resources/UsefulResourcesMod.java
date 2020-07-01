1bf6f3707a335a915ca0137a5597c73fbd8bebd9
HyCraftHD <hycrafthd@live.de>
2020-07-01 15:35:33 +0200

    Register useful resources with bus registry

--- src/main/java/info/u_team/useful_resources/UsefulResourcesMod.java
+++ src/main/java/info/u_team/useful_resources/UsefulResourcesMod.java
@@ -1,7 +1,9 @@
 package info.u_team.useful_resources;
 
+import info.u_team.u_team_core.util.registry.BusRegister;
 import info.u_team.u_team_core.util.verify.JarSignVerifier;
 import info.u_team.useful_resources.config.CommonConfig;
+import info.u_team.useful_resources.init.UsefulResourcesResources;
 import info.u_team.useful_resources.resources.Resources;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
@@ -15,6 +17,11 @@ public class UsefulResourcesMod {
 	public UsefulResourcesMod() {
 		JarSignVerifier.checkSigned(MODID);
 		ModLoadingContext.get().registerConfig(Type.COMMON, CommonConfig.CONFIG);
+		register();
+	}
+	
+	private void register() {
 		Resources.register();
+		BusRegister.registerMod(UsefulResourcesResources::register);
 	}
 }
