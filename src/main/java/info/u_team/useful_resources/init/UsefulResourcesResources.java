d5decb13b00a262732a3af2ad2dd1c3c4cff12db
HyCraftHD <hycrafthd@live.de>
2020-07-01 15:35:42 +0200

    Create useful resources class

---
+++ src/main/java/info/u_team/useful_resources/init/UsefulResourcesResources.java
@@ -0,0 +1,10 @@
+package info.u_team.useful_resources.init;
+
+import net.minecraftforge.eventbus.api.IEventBus;
+
+public class UsefulResourcesResources {
+	
+	public static void register(IEventBus bus) {
+	}
+	
+}
