e07201b2d1e677525aa8ec74beb12f1b5327d2de
HyCraftHD <hycrafthd@live.de>
2020-07-01 15:46:23 +0200

    Fix particle registry

--- src/main/java/info/u_team/useful_resources/init/UsefulResourcesParticleTypes.java
+++ src/main/java/info/u_team/useful_resources/init/UsefulResourcesParticleTypes.java
@@ -1,12 +1,10 @@
 package info.u_team.useful_resources.init;
 
-import info.u_team.u_team_core.particletype.UParticleType;
-import info.u_team.u_team_core.util.registry.*;
+import info.u_team.u_team_core.util.registry.CommonDeferredRegister;
 import info.u_team.useful_resources.UsefulResourcesMod;
-import info.u_team.useful_resources.api.resource.IResource;
 import net.minecraft.particles.*;
-import net.minecraftforge.event.RegistryEvent.Register;
-import net.minecraftforge.eventbus.api.*;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -16,7 +14,7 @@ public class UsefulResourcesParticleTypes {
 	
 	public static final CommonDeferredRegister<ParticleType<?>> PARTICLE_TYPES = CommonDeferredRegister.create(ForgeRegistries.PARTICLE_TYPES, UsefulResourcesMod.MODID);
 	
-	public static final UParticleType<BlockParticleData> COLORED_OVERLAY_BLOCK = new UParticleType<>("colored_overlay_block", false, BlockParticleData.DESERIALIZER);
+	public static final RegistryObject<ParticleType<BlockParticleData>> COLORED_OVERLAY_BLOCK = PARTICLE_TYPES.register("colored_overlay_block", () -> new ParticleType<>(false, BlockParticleData.DESERIALIZER));
 	
 	public static void register(IEventBus bus) {
 		PARTICLE_TYPES.register(bus);
