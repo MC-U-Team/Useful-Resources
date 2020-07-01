fcf366bd73188f3c7c01653d023f75b9d4e78456
HyCraftHD <hycrafthd@live.de>
2020-07-01 15:58:34 +0200

    Fix loot table provider

--- src/main/java/info/u_team/useful_resources/data/provider/ResourceLootTableProvider.java
+++ src/main/java/info/u_team/useful_resources/data/provider/ResourceLootTableProvider.java
@@ -1,7 +1,7 @@
 package info.u_team.useful_resources.data.provider;
 
 import java.util.Map;
-import java.util.function.BiConsumer;
+import java.util.function.*;
 
 import info.u_team.u_team_core.data.*;
 import info.u_team.useful_resources.api.ResourceRegistry;
@@ -20,11 +20,11 @@ public class ResourceLootTableProvider extends CommonLootTablesProvider {
 		ResourceRegistry.getResources().forEach(resource -> {
 			resource.iterateRegistryBlocks((type, block) -> {
 				final LootTable lootTable;
-				final Map<BlockResourceType, LootTable> extraLootTables = resource.getDataGeneratorConfigurator().getExtraLootTables();
+				final Map<BlockResourceType, Supplier<LootTable>> extraLootTables = resource.getDataGeneratorConfigurator().getExtraLootTables();
 				if (block.getLootTable().equals(LootTables.EMPTY)) {
 					lootTable = null;
 				} else if (extraLootTables.containsKey(type)) {
-					lootTable = extraLootTables.get(type);
+					lootTable = extraLootTables.get(type).get();
 				} else {
 					lootTable = addBasicBlockLootTable(block);
 				}
