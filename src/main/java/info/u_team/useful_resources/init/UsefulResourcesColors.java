776ea81c4ff11deb334f24b8cdedbe831f2856ea
HyCraftHD <hycrafthd@live.de>
2020-11-16 21:29:44 +0100

    Use the new system for registering stuff

--- src/main/java/info/u_team/useful_resources/init/UsefulResourcesColors.java
+++ src/main/java/info/u_team/useful_resources/init/UsefulResourcesColors.java
@@ -2,22 +2,16 @@ package info.u_team.useful_resources.init;
 
 import com.google.common.collect.Streams;
 
-import info.u_team.useful_resources.UsefulResourcesMod;
 import info.u_team.useful_resources.api.ResourceRegistry;
 import info.u_team.useful_resources.api.registry.RegistryEntry;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.ColorHandlerEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.eventbus.api.IEventBus;
 
-@EventBusSubscriber(modid = UsefulResourcesMod.MODID, bus = Bus.MOD, value = Dist.CLIENT)
 public class UsefulResourcesColors {
 	
-	@SubscribeEvent
-	public static void register(ColorHandlerEvent.Item event) {
+	private static void colorItem(ColorHandlerEvent.Item event) {
 		ResourceRegistry.getResources().forEach(resource -> {
 			event.getBlockColors().register((state, world, pos, tintIndex) -> {
 				if (tintIndex == 1) {
@@ -37,6 +31,10 @@ public class UsefulResourcesColors {
 				}
 			}, Streams.concat(resource.getRegistryBlocks().stream().map(RegistryEntry::get).map(Block::asItem), resource.getRegistryItems().stream().map(RegistryEntry::get)).toArray(Item[]::new));
 		});
+	}
+	
+	public static void registerMod(IEventBus bus) {
+		bus.addListener(UsefulResourcesColors::colorItem);
 	}
 	
 }
