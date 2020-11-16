008171af828e0df325357f3a0fef1e58f242a64e
HyCraftHD <hycrafthd@live.de>
2020-11-16 21:19:12 +0100

    Use annotationsmanager to call the common construct now

--- src/main/java/info/u_team/useful_resources/UsefulResourcesMod.java
+++ src/main/java/info/u_team/useful_resources/UsefulResourcesMod.java
@@ -1,13 +1,8 @@
 package info.u_team.useful_resources;
 
-import info.u_team.u_team_core.util.registry.BusRegister;
+import info.u_team.u_team_core.util.annotation.AnnotationManager;
 import info.u_team.u_team_core.util.verify.JarSignVerifier;
-import info.u_team.useful_resources.config.CommonConfig;
-import info.u_team.useful_resources.init.*;
-import info.u_team.useful_resources.resources.Resources;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig.Type;
 
 @Mod(UsefulResourcesMod.MODID)
 public class UsefulResourcesMod {
@@ -16,13 +11,7 @@ public class UsefulResourcesMod {
 	
 	public UsefulResourcesMod() {
 		JarSignVerifier.checkSigned(MODID);
-		ModLoadingContext.get().registerConfig(Type.COMMON, CommonConfig.CONFIG);
-		register();
-	}
-	
-	private void register() {
-		Resources.register();
-		BusRegister.registerMod(UsefulResourcesParticleTypes::register);
-		BusRegister.registerMod(UsefulResourcesResources::register);
+		
+		AnnotationManager.callAnnotations(MODID);
 	}
 }
