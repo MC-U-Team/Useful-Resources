1a63cebf8d10fd9feb66d276d1557f09ffcafe73
HyCraftHD <hycrafthd@live.de>
2020-11-16 21:22:02 +0100

    Make stuff private, remove unused annotation

--- src/main/java/info/u_team/useful_resources/init/UsefulResourcesParticleTypes.java
+++ src/main/java/info/u_team/useful_resources/init/UsefulResourcesParticleTypes.java
@@ -7,14 +7,11 @@ import info.u_team.useful_resources.UsefulResourcesMod;
 import net.minecraft.particles.*;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.RegistryObject;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.registries.ForgeRegistries;
 
-@EventBusSubscriber(modid = UsefulResourcesMod.MODID, bus = Bus.MOD)
 public class UsefulResourcesParticleTypes {
 	
-	public static final CommonDeferredRegister<ParticleType<?>> PARTICLE_TYPES = CommonDeferredRegister.create(ForgeRegistries.PARTICLE_TYPES, UsefulResourcesMod.MODID);
+	private static final CommonDeferredRegister<ParticleType<?>> PARTICLE_TYPES = CommonDeferredRegister.create(ForgeRegistries.PARTICLE_TYPES, UsefulResourcesMod.MODID);
 	
 	public static final RegistryObject<ParticleType<BlockParticleData>> COLORED_OVERLAY_BLOCK = PARTICLE_TYPES.register("colored_overlay_block", () -> new ParticleType<BlockParticleData>(false, BlockParticleData.DESERIALIZER) {
 		
