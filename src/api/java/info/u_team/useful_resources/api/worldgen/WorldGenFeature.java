ae20f5965c4236e1310d9cbc6468843983438c40
HyCraftHD <hycrafthd@live.de>
2020-11-19 13:58:51 +0100

    Add codec wip to world gen feature

--- src/api/java/info/u_team/useful_resources/api/worldgen/WorldGenFeature.java
+++ src/api/java/info/u_team/useful_resources/api/worldgen/WorldGenFeature.java
@@ -1,27 +1,36 @@
 package info.u_team.useful_resources.api.worldgen;
-/*
-import java.util.Objects;
 
-import com.google.common.collect.ImmutableMap;
-import com.mojang.serialization.*;
+import java.util.*;
+import java.util.function.Supplier;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 
 import net.minecraft.world.gen.GenerationStage.Decoration;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 
 public class WorldGenFeature implements IWorldGenFeature {
 	
+	public static final Codec<WorldGenFeature> CODEC = RecordCodecBuilder.create(instance -> {
+		return instance.group( //
+				CategoryTypeList.CODEC.fieldOf("categories").forGetter(IWorldGenFeature::getCategories), //
+				BiomeTypeList.CODEC.fieldOf("biomes").forGetter(IWorldGenFeature::getBiomes), //
+				ConfiguredFeature.field_242764_c.fieldOf("feature").forGetter(IWorldGenFeature::getFeatures) //
+		).apply(instance, WorldGenFeature::new);
+	});
+	
 	private final CategoryTypeList categories;
 	private final BiomeTypeList biomes;
 	
-	private final Decoration decoration;
+	// private final Decoration decoration;
 	
-	private final ConfiguredFeature<?, ?> feature;
+	private final List<Supplier<ConfiguredFeature<?, ?>>> features;
 	
-	public WorldGenFeature(CategoryTypeList categories, BiomeTypeList biomes, Decoration decoration, ConfiguredFeature<?, ?> feature) {
+	public WorldGenFeature(CategoryTypeList categories, BiomeTypeList biomes, /* Decoration decoration, */ List<Supplier<ConfiguredFeature<?, ?>>> features) {
 		this.categories = Objects.requireNonNull(categories);
 		this.biomes = Objects.requireNonNull(biomes);
-		this.decoration = Objects.requireNonNull(decoration);
-		this.feature = Objects.requireNonNull(feature);
+		// this.decoration = Objects.requireNonNull(decoration);
+		this.features = Objects.requireNonNull(features);
 	}
 	
 	@Override
@@ -36,24 +45,29 @@ public class WorldGenFeature implements IWorldGenFeature {
 	
 	@Override
 	public Decoration getDecoration() {
-		return decoration;
+		return null;
+		// return decoration;
 	}
 	
 	@Override
-	public ConfiguredFeature<?, ?> getFeature() {
-		return feature;
+	public List<Supplier<ConfiguredFeature<?, ?>>> getFeatures() {
+		return features;
 	}
 	
-	public <T> Dynamic<T> serialize(DynamicOps<T> ops) {
-		return new Dynamic<>(ops, ops.createMap(ImmutableMap.of(ops.createString("categories"), categories.serialize(ops).getValue(), ops.createString("biomes"), biomes.serialize(ops).getValue(), ops.createString("decoration"), ops.createString(decoration.getName()), ops.createString("feature"), feature.serialize(ops).getValue())));
-	}
+	// public <T> Dynamic<T> serialize(DynamicOps<T> ops) {
+	// return new Dynamic<>(ops, ops.createMap(ImmutableMap.of(ops.createString("categories"),
+	// categories.serialize(ops).getValue(), ops.createString("biomes"), biomes.serialize(ops).getValue(),
+	// ops.createString("decoration"), ops.createString(decoration.getName()), ops.createString("feature"),
+	// feature.serialize(ops).getValue())));
+	// }
+	//
+	// public static <T> WorldGenFeature deserialize(Dynamic<T> ops) {
+	// final CategoryTypeList categories = CategoryTypeList.deserialize(ops.get("categories").orElseEmptyMap());
+	// final BiomeTypeList biomes = BiomeTypeList.deserialize(ops.get("biomes").orElseEmptyMap());
+	// final Decoration decoration = Decoration.BY_NAME.getOrDefault(ops.get("decoration").asString(""),
+	// Decoration.RAW_GENERATION);
+	// final ConfiguredFeature<?, ?> feature = ConfiguredFeature.deserialize(ops.get("feature").orElseEmptyMap());
+	// return new WorldGenFeature(categories, biomes, decoration, feature);
+	// }
 	
-	public static <T> WorldGenFeature deserialize(Dynamic<T> ops) {
-		final CategoryTypeList categories = CategoryTypeList.deserialize(ops.get("categories").orElseEmptyMap());
-		final BiomeTypeList biomes = BiomeTypeList.deserialize(ops.get("biomes").orElseEmptyMap());
-		final Decoration decoration = Decoration.BY_NAME.getOrDefault(ops.get("decoration").asString(""), Decoration.RAW_GENERATION);
-		final ConfiguredFeature<?, ?> feature = ConfiguredFeature.deserialize(ops.get("feature").orElseEmptyMap());
-		return new WorldGenFeature(categories, biomes, decoration, feature);
-	}
-	
-}*/
+}
