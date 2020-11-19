2b04cba7b38e1968e6534eab6bb71f490775d55c
HyCraftHD <hycrafthd@live.de>
2020-11-19 13:46:34 +0100

    Add codec to category type list

--- src/api/java/info/u_team/useful_resources/api/worldgen/CategoryTypeList.java
+++ src/api/java/info/u_team/useful_resources/api/worldgen/CategoryTypeList.java
@@ -1,26 +1,24 @@
 package info.u_team.useful_resources.api.worldgen;
-/*
-import java.util.List;
-import java.util.stream.Collectors;
 
-import com.google.common.collect.ImmutableMap;
-import com.mojang.serialization.*;
+import java.util.List;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 
 import info.u_team.useful_resources.api.list.*;
 import net.minecraft.world.biome.Biome.Category;
 
 public class CategoryTypeList extends TypeList<Category> {
 	
+	public static final Codec<CategoryTypeList> CODEC = RecordCodecBuilder.create(instance -> {
+		return instance.group( //
+				ListType.CODEC.fieldOf("type").forGetter(TypeList::getType), //
+				Codec.list(Category.CODEC).fieldOf("categories").forGetter(TypeList::getList) //
+		).apply(instance, CategoryTypeList::new);
+	});
+	
 	public CategoryTypeList(ListType type, List<Category> list) {
 		super(type, list);
 	}
 	
-	public <T> Dynamic<T> serialize(DynamicOps<T> ops) {
-		return new Dynamic<>(ops, ops.createMap(ImmutableMap.of(ops.createString("type"), ops.createString(getType().getName()), ops.createString("entries"), ops.createList(getList().stream().map(entry -> ops.createString(entry.getName()))))));
-	}
-	
-	public static <T> CategoryTypeList deserialize(Dynamic<T> ops) {
-		return new CategoryTypeList(ListType.byName(ops.get("type").asString("")), ops.get("entries").asStream().map(entry -> entry.asString("")).filter(Category.BY_NAME::containsKey).map(Category.BY_NAME::get).collect(Collectors.toList()));
-	}
-	
-}*/
+}
