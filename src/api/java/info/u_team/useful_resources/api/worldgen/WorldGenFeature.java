ec5d97defefa90f4950c2e2da972b2f9b08832a3
HyCraftHD <hycrafthd@live.de>
2020-11-19 14:21:46 +0100

    Add codec to the world gen feature

--- src/api/java/info/u_team/useful_resources/api/worldgen/WorldGenFeature.java
+++ src/api/java/info/u_team/useful_resources/api/worldgen/WorldGenFeature.java
@@ -6,6 +6,7 @@ import java.util.function.Supplier;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 
+import info.u_team.useful_resources.api.util.GeneralEnumCodec;
 import net.minecraft.world.gen.GenerationStage.Decoration;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 
@@ -15,6 +16,7 @@ public class WorldGenFeature implements IWorldGenFeature {
 		return instance.group( //
 				CategoryTypeList.CODEC.fieldOf("categories").forGetter(IWorldGenFeature::getCategories), //
 				BiomeTypeList.CODEC.fieldOf("biomes").forGetter(IWorldGenFeature::getBiomes), //
+				GeneralEnumCodec.createCodec(Decoration.class).fieldOf("decoration").forGetter(IWorldGenFeature::getDecoration), //
 				ConfiguredFeature.field_242764_c.fieldOf("feature").forGetter(IWorldGenFeature::getFeatures) //
 		).apply(instance, WorldGenFeature::new);
 	});
@@ -22,14 +24,14 @@ public class WorldGenFeature implements IWorldGenFeature {
 	private final CategoryTypeList categories;
 	private final BiomeTypeList biomes;
 	
-	// private final Decoration decoration;
+	private final Decoration decoration;
 	
 	private final List<Supplier<ConfiguredFeature<?, ?>>> features;
 	
-	public WorldGenFeature(CategoryTypeList categories, BiomeTypeList biomes, /* Decoration decoration, */ List<Supplier<ConfiguredFeature<?, ?>>> features) {
+	public WorldGenFeature(CategoryTypeList categories, BiomeTypeList biomes, Decoration decoration, List<Supplier<ConfiguredFeature<?, ?>>> features) {
 		this.categories = Objects.requireNonNull(categories);
 		this.biomes = Objects.requireNonNull(biomes);
-		// this.decoration = Objects.requireNonNull(decoration);
+		this.decoration = Objects.requireNonNull(decoration);
 		this.features = Objects.requireNonNull(features);
 	}
 	
@@ -45,8 +47,7 @@ public class WorldGenFeature implements IWorldGenFeature {
 	
 	@Override
 	public Decoration getDecoration() {
-		return null;
-		// return decoration;
+		return decoration;
 	}
 	
 	@Override
