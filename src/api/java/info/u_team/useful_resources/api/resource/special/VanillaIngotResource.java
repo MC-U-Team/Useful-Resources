78b755abd5baf674d9877ef685683dd82b237487
HyCraftHD <hycrafthd@live.de>
2020-11-25 17:39:08 +0100

    Add way to add ingot resource without an ore

--- src/api/java/info/u_team/useful_resources/api/resource/special/VanillaIngotResource.java
+++ src/api/java/info/u_team/useful_resources/api/resource/special/VanillaIngotResource.java
@@ -1,19 +1,25 @@
 package info.u_team.useful_resources.api.resource.special;
 
 import static info.u_team.useful_resources.api.resource.CommonResourceBuilder.*;
-import static info.u_team.useful_resources.api.type.BlockResourceType.NETHER_ORE;
 import static info.u_team.useful_resources.api.type.ItemResourceType.*;
 
 import info.u_team.useful_resources.api.resource.BasicResource;
 import info.u_team.useful_resources.api.resource.data.IDataGeneratorConfigurator.ResourceType;
-import info.u_team.useful_resources.api.type.ItemResourceType;
+import info.u_team.useful_resources.api.type.*;
 import net.minecraft.item.Rarity;
 
 public class VanillaIngotResource extends BasicResource<VanillaIngotResource> {
 	
 	public VanillaIngotResource(String name, int color, int harvestLevel, float hardness, float resistance) {
+		this(name, color, harvestLevel, hardness, resistance, null);
+	}
+	
+	public VanillaIngotResource(String name, int color, int harvestLevel, float hardness, float resistance, BlockResourceType oreType) {
 		super(name, color, ItemResourceType.INGOT, Rarity.COMMON, ResourceType.INGOT);
-		addFeature(createOre(NETHER_ORE, Rarity.COMMON, harvestLevel, hardness, resistance));
+		
+		if (oreType != null) {
+			addFeature(createOre(oreType, Rarity.COMMON, harvestLevel, hardness, resistance));
+		}
 		
 		addFeature(createMoltenFluid(0xFF000000 + color));
 		
