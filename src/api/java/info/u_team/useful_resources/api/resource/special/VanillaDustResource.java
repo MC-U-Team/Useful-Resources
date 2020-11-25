226f89aaa196dbe2722a4fb197b7814070309113
HyCraftHD <hycrafthd@live.de>
2020-11-25 21:15:41 +0100

    Change dust resource like the gem resources

--- src/api/java/info/u_team/useful_resources/api/resource/special/VanillaDustResource.java
+++ src/api/java/info/u_team/useful_resources/api/resource/special/VanillaDustResource.java
@@ -4,21 +4,30 @@ import static info.u_team.useful_resources.api.resource.CommonResourceBuilder.*;
 import static info.u_team.useful_resources.api.type.BlockResourceType.*;
 import static info.u_team.useful_resources.api.type.ItemResourceType.*;
 
+import org.apache.commons.lang3.ArrayUtils;
+
 import info.u_team.useful_resources.api.resource.BasicResource;
 import info.u_team.useful_resources.api.resource.data.IDataGeneratorConfigurator.ResourceType;
-import info.u_team.useful_resources.api.type.ItemResourceType;
+import info.u_team.useful_resources.api.type.*;
 import net.minecraft.item.Rarity;
 import net.minecraft.util.math.MathHelper;
 
 public class VanillaDustResource extends BasicResource<VanillaDustResource> {
 	
-	public VanillaDustResource(String name, int color, int harvestLevel, float hardness, float resistance, int minXp, int maxXp) {
+	public VanillaDustResource(String name, int color, int harvestLevel, float baseHardness, int minXp, int maxXp, BlockResourceType... existingOres) {
 		super(name, color, ItemResourceType.DUST, Rarity.COMMON, ResourceType.DUST);
 		
 		setProperty("ingotModel", true);
 		
-		addFeature(createOre(NETHER_ORE, Rarity.COMMON, harvestLevel, hardness, resistance, random -> MathHelper.nextInt(random, minXp, maxXp)));
-		addFeature(createOre(END_ORE, Rarity.COMMON, harvestLevel, hardness * 1.5F, resistance * 2, random -> MathHelper.nextInt(random, minXp, maxXp)));
+		if (!ArrayUtils.contains(existingOres, ORE)) {
+			addFeature(createOre(ORE, Rarity.COMMON, harvestLevel, baseHardness, baseHardness, random -> MathHelper.nextInt(random, minXp, maxXp)));
+		}
+		if (!ArrayUtils.contains(existingOres, NETHER_ORE)) {
+			addFeature(createOre(NETHER_ORE, Rarity.COMMON, harvestLevel, baseHardness, baseHardness, random -> MathHelper.nextInt(random, minXp, maxXp)));
+		}
+		if (!ArrayUtils.contains(existingOres, END_ORE)) {
+			addFeature(createOre(END_ORE, Rarity.COMMON, harvestLevel, baseHardness * 1.5F, baseHardness * 2, random -> MathHelper.nextInt(random, minXp, maxXp)));
+		}
 		
 		addFeature(createMoltenFluid(0xFF000000 + color));
 		
