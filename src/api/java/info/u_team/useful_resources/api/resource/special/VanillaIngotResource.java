8edf0778181c4b728789adae620c4e27e2ee7802
HyCraftHD <hycrafthd@live.de>
2020-11-25 20:58:55 +0100

    Restructure vanilla ingot resource with existing stuff and ore stuff a bit

--- src/api/java/info/u_team/useful_resources/api/resource/special/VanillaIngotResource.java
+++ src/api/java/info/u_team/useful_resources/api/resource/special/VanillaIngotResource.java
@@ -1,7 +1,10 @@
 package info.u_team.useful_resources.api.resource.special;
 
 import static info.u_team.useful_resources.api.resource.CommonResourceBuilder.*;
+import static info.u_team.useful_resources.api.type.BlockResourceType.*;
 import static info.u_team.useful_resources.api.type.ItemResourceType.*;
+
+import org.apache.commons.lang3.ArrayUtils;
 
 import info.u_team.useful_resources.api.resource.BasicResource;
 import info.u_team.useful_resources.api.resource.data.IDataGeneratorConfigurator.ResourceType;
@@ -10,15 +13,17 @@ import net.minecraft.item.Rarity;
 
 public class VanillaIngotResource extends BasicResource<VanillaIngotResource> {
 	
-	public VanillaIngotResource(String name, int color, int harvestLevel, float hardness, float resistance) {
-		this(name, color, harvestLevel, hardness, resistance, null);
-	}
-	
-	public VanillaIngotResource(String name, int color, int harvestLevel, float hardness, float resistance, BlockResourceType oreType) {
+	public VanillaIngotResource(String name, int color, int harvestLevel, float baseHardness, BlockResourceType... existingOres) {
 		super(name, color, ItemResourceType.INGOT, Rarity.COMMON, ResourceType.INGOT);
 		
-		if (oreType != null) {
-			addFeature(createOre(oreType, Rarity.COMMON, harvestLevel, hardness, resistance));
+		if (!ArrayUtils.contains(existingOres, ORE)) {
+			addFeature(createOre(ORE, Rarity.COMMON, harvestLevel, baseHardness * 0.75F, baseHardness * 1.25F));
+		}
+		if (!ArrayUtils.contains(existingOres, NETHER_ORE)) {
+			addFeature(createOre(NETHER_ORE, Rarity.COMMON, harvestLevel, baseHardness * 0.75F, baseHardness * 1.25F));
+		}
+		if (!ArrayUtils.contains(existingOres, END_ORE)) {
+			addFeature(createOre(END_ORE, Rarity.COMMON, harvestLevel, baseHardness * 1.5F, baseHardness * 2));
 		}
 		
 		addFeature(createMoltenFluid(0xFF000000 + color));
