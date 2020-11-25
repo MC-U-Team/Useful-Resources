b879445ebfd63ad0eeff09e8d6780e6d038de972
HyCraftHD <hycrafthd@live.de>
2020-11-25 19:00:05 +0100

    Add some more arguments

--- src/main/java/info/u_team/useful_resources/block/OreBlock.java
+++ src/main/java/info/u_team/useful_resources/block/OreBlock.java
@@ -5,7 +5,7 @@ import java.util.function.Function;
 
 import info.u_team.useful_resources.init.UsefulResourcesItemGroups;
 import net.minecraft.block.*;
-import net.minecraft.block.material.Material;
+import net.minecraft.block.material.*;
 import net.minecraft.item.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IWorldReader;
@@ -15,8 +15,8 @@ public class OreBlock extends ParticleBlock {
 	
 	private final Function<Random, Integer> experienceDrop;
 	
-	public OreBlock(Rarity rarity, int harvestLevel, float hardness, float resistance, Function<Random, Integer> experienceDrop) {
-		super(UsefulResourcesItemGroups.GROUP, Properties.create(Material.ROCK).sound(SoundType.STONE).harvestTool(ToolType.PICKAXE).harvestLevel(harvestLevel).setRequiresTool().hardnessAndResistance(hardness, resistance), new Item.Properties().rarity(rarity));
+	public OreBlock(MaterialColor color, SoundType soundType, Rarity rarity, int harvestLevel, float hardness, float resistance, Function<Random, Integer> experienceDrop) {
+		super(UsefulResourcesItemGroups.GROUP, Properties.create(Material.ROCK).sound(soundType).harvestTool(ToolType.PICKAXE).harvestLevel(harvestLevel).setRequiresTool().hardnessAndResistance(hardness, resistance), new Item.Properties().rarity(rarity));
 		this.experienceDrop = experienceDrop;
 	}
 	
