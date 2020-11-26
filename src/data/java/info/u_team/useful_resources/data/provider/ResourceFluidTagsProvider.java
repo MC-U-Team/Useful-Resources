2e0ac96b7cf756f5fd24dd91b957a3db5a9ee79e
HyCraftHD <hycrafthd@live.de>
2020-11-26 14:59:40 +0100

    Should add the tags for aluminium moltens

--- src/data/java/info/u_team/useful_resources/data/provider/ResourceFluidTagsProvider.java
+++ src/data/java/info/u_team/useful_resources/data/provider/ResourceFluidTagsProvider.java
@@ -2,6 +2,8 @@ package info.u_team.useful_resources.data.provider;
 
 import info.u_team.u_team_core.data.*;
 import info.u_team.useful_resources.api.ResourceRegistry;
+import info.u_team.useful_resources.resources.Resources;
+import info.u_team.useful_resources.util.FakeNameResource;
 import net.minecraft.fluid.Fluid;
 import net.minecraft.tags.ITag.INamedTag;
 
@@ -23,6 +25,20 @@ public class ResourceFluidTagsProvider extends CommonFluidTagsProvider {
 					}
 				}
 			});
+		});
+		
+		// Special tags
+		
+		// Add aluminum to the aluminium tag
+		final FakeNameResource aluminium = new FakeNameResource("aluminium", Resources.ALUMINUM);
+		aluminium.iterateRegistryFluids((type, fluid) -> {
+			if (type.hasTag()) {
+				final INamedTag<Fluid> tag = type.getTag(aluminium);
+				getBuilder(tag).add(fluid);
+				if (type.hasUnifyTag()) {
+					getBuilder(type.getUnifyTag()).add(tag);
+				}
+			}
 		});
 	}
 }
