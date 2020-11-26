d3b0078c4c5c35e250c21e22b2b49021ff9b59a1
HyCraftHD <hycrafthd@live.de>
2020-11-26 01:24:21 +0100

    Refactor some stuff

--- src/main/java/info/u_team/useful_resources/util/GenerationUtil.java
+++ src/main/java/info/u_team/useful_resources/util/GenerationUtil.java
@@ -26,41 +26,41 @@ public class GenerationUtil {
 	// ----------------------- Generation methods for basic stuff ------------------------------- //
 	
 	public static WorldGenFeatures createOreFeatureRangeOverworld(BlockState state, int size, int count, int bottomOffset, int topOffset, int maximum) {
-		return createOreFeatureRange(OVERWORLD_FILTER, FillerBlockType.BASE_STONE_OVERWORLD, state, size, count, bottomOffset, topOffset, maximum);
+		return createOreFeatureRange(OVERWORLD_FILTER, FillerBlockType.BASE_STONE_OVERWORLD, state, size, count, bottomOffset, topOffset, maximum, Optional.empty());
 	}
 	
 	public static WorldGenFeatures createOreFeatureRangeNether(BlockState state, int size, int count, int bottomOffset, int topOffset, int maximum) {
-		return createOreFeatureRange(NETHER_FILTER, NETHER_RULE, state, size, count, bottomOffset, topOffset, maximum);
+		return createOreFeatureRange(NETHER_FILTER, NETHER_RULE, state, size, count, bottomOffset, topOffset, maximum, Optional.empty());
 	}
 	
 	public static WorldGenFeatures createOreFeatureRangeEnd(BlockState state, int size, int count, int bottomOffset, int topOffset, int maximum) {
-		return createOreFeatureRange(END_FILTER, END_RULE, state, size, count, bottomOffset, topOffset, maximum);
+		return createOreFeatureRange(END_FILTER, END_RULE, state, size, count, bottomOffset, topOffset, maximum, Optional.empty());
 	}
 	
-	public static WorldGenFeatures createOreFeatureRange(FilterTypeLists filterTypeLists, RuleTest fillerBlockType, BlockState state, int size, int count, int bottomOffset, int topOffset, int maximum) {
-		return createOreFeature(filterTypeLists, fillerBlockType, state, size, count, feature -> feature.withPlacement(Placement.RANGE.configure(new TopSolidRangeConfig(bottomOffset, topOffset, maximum))));
+	public static WorldGenFeatures createOreFeatureRange(FilterTypeLists filterTypeLists, RuleTest fillerBlockType, BlockState state, int size, int count, int bottomOffset, int topOffset, int maximum, Optional<List<ConfiguredPlacement<?>>> extraPlacements) {
+		return createOreFeature(filterTypeLists, fillerBlockType, state, size, count, feature -> feature.withPlacement(Placement.RANGE.configure(new TopSolidRangeConfig(bottomOffset, topOffset, maximum))), extraPlacements);
 	}
 	
 	public static WorldGenFeatures createOreFeatureDepthAverageOverworld(BlockState state, int size, int count, int baseline, int spread) {
-		return createOreFeatureDepthAverage(OVERWORLD_FILTER, FillerBlockType.BASE_STONE_OVERWORLD, state, size, count, baseline, spread);
+		return createOreFeatureDepthAverage(OVERWORLD_FILTER, FillerBlockType.BASE_STONE_OVERWORLD, state, size, count, baseline, spread, Optional.empty());
 	}
 	
 	public static WorldGenFeatures createOreFeatureDepthAverageNether(BlockState state, int size, int count, int baseline, int spread) {
-		return createOreFeatureDepthAverage(NETHER_FILTER, NETHER_RULE, state, size, count, baseline, spread);
+		return createOreFeatureDepthAverage(NETHER_FILTER, NETHER_RULE, state, size, count, baseline, spread, Optional.empty());
 	}
 	
 	public static WorldGenFeatures createOreFeatureDepthAverageEnd(BlockState state, int size, int count, int baseline, int spread) {
-		return createOreFeatureDepthAverage(END_FILTER, END_RULE, state, size, count, baseline, spread);
+		return createOreFeatureDepthAverage(END_FILTER, END_RULE, state, size, count, baseline, spread, Optional.empty());
 	}
 	
-	public static WorldGenFeatures createOreFeatureDepthAverage(FilterTypeLists filterTypeLists, RuleTest fillerBlockType, BlockState state, int size, int count, int baseline, int spread) {
-		return createOreFeature(filterTypeLists, fillerBlockType, state, size, count, feature -> feature.withPlacement(Placement.DEPTH_AVERAGE.configure(new DepthAverageConfig(baseline, spread))));
+	public static WorldGenFeatures createOreFeatureDepthAverage(FilterTypeLists filterTypeLists, RuleTest fillerBlockType, BlockState state, int size, int count, int baseline, int spread, Optional<List<ConfiguredPlacement<?>>> extraPlacements) {
+		return createOreFeature(filterTypeLists, fillerBlockType, state, size, count, feature -> feature.withPlacement(Placement.DEPTH_AVERAGE.configure(new DepthAverageConfig(baseline, spread))), extraPlacements);
 	}
 	
 	// ----------------------- ---------------------------------- ------------------------------- //
 	
-	public static WorldGenFeatures createOreFeature(FilterTypeLists filterTypeLists, RuleTest fillerBlockType, BlockState state, int size, int count, UnaryOperator<ConfiguredFeature<?, ?>> decoratable) {
-		return createOreFeature(filterTypeLists, fillerBlockType, state, size, feature -> decoratable.apply(feature).square().func_242731_b(count), Optional.empty());
+	public static WorldGenFeatures createOreFeature(FilterTypeLists filterTypeLists, RuleTest fillerBlockType, BlockState state, int size, int count, UnaryOperator<ConfiguredFeature<?, ?>> decoratable, Optional<List<ConfiguredPlacement<?>>> extraPlacements) {
+		return createOreFeature(filterTypeLists, fillerBlockType, state, size, feature -> decoratable.apply(feature).square().func_242731_b(count), extraPlacements);
 	}
 	
 	public static WorldGenFeatures createOreFeature(FilterTypeLists filterTypeLists, RuleTest fillerBlockType, BlockState state, int size, UnaryOperator<ConfiguredFeature<?, ?>> decoratable, Optional<List<ConfiguredPlacement<?>>> extraPlacements) {
