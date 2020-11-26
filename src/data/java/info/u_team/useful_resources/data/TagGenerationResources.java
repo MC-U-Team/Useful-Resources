f77e7863fdb98209d6c2f6582dd9c850a8ef7942
HyCraftHD <hycrafthd@live.de>
2020-11-26 23:36:59 +0100

    Add tag generation resources

---
+++ src/data/java/info/u_team/useful_resources/data/TagGenerationResources.java
@@ -0,0 +1,18 @@
+package info.u_team.useful_resources.data;
+
+import java.util.*;
+
+import info.u_team.useful_resources.api.ResourceRegistry;
+import info.u_team.useful_resources.api.resource.IResource;
+import info.u_team.useful_resources.resources.Resources;
+import info.u_team.useful_resources.util.FakeNameResource;
+import net.minecraft.util.Util;
+
+public class TagGenerationResources {
+	
+	public static final List<IResource> RESOURCES = Util.make(new ArrayList<>(), list -> ResourceRegistry.getResources().forEach(list::add));
+	
+	public static void addAdditionalResources() {
+		RESOURCES.add(new FakeNameResource("aluminium", Resources.ALUMINUM));
+	}
+}
