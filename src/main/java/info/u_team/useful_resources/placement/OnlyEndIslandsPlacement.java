556eb318f8bb28fca69bbd36d2249b3d69abd09f
HyCraftHD <hycrafthd@live.de>
2020-11-26 01:21:02 +0100

    Implement logic, if is 500 blocks range no placement will occur for the end for 0 0 0

--- src/main/java/info/u_team/useful_resources/placement/OnlyEndIslandsPlacement.java
+++ src/main/java/info/u_team/useful_resources/placement/OnlyEndIslandsPlacement.java
@@ -4,6 +4,7 @@ import java.util.Random;
 import java.util.stream.Stream;
 
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.vector.Vector3i;
 import net.minecraft.world.gen.placement.*;
 
 public class OnlyEndIslandsPlacement extends SimplePlacement<NoPlacementConfig> {
@@ -14,7 +15,9 @@ public class OnlyEndIslandsPlacement extends SimplePlacement<NoPlacementConfig> 
 	
 	@Override
 	protected Stream<BlockPos> getPositions(Random random, NoPlacementConfig config, BlockPos pos) {
-		return null;
+		if (pos.withinDistance(Vector3i.NULL_VECTOR, 500)) {
+			return Stream.empty();
+		}
+		return Stream.of(pos);
 	}
-	
 }
