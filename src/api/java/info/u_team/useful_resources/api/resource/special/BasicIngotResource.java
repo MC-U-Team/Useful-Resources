7943e2df3c0b5e69763fa06489dbdc08dd6b3bed
HyCraftHD <hycrafthd@live.de>
2020-11-27 18:03:41 +0100

    Add bars to ingot resource

--- src/api/java/info/u_team/useful_resources/api/resource/special/BasicIngotResource.java
+++ src/api/java/info/u_team/useful_resources/api/resource/special/BasicIngotResource.java
@@ -17,6 +17,7 @@ public class BasicIngotResource extends BasicResource<BasicIngotResource> {
 		addFeature(createOre(NETHER_ORE, rarity, harvestLevel, baseHardness * 0.75F, baseHardness * 1.25F));
 		addFeature(createOre(END_ORE, rarity, harvestLevel, baseHardness * 1.5F, baseHardness * 2));
 		addFeature(createBasicBlock(BLOCK, rarity, harvestLevel, baseHardness * 2, baseHardness * 4));
+		addFeature(createBars(rarity, harvestLevel, baseHardness));
 		
 		addFeature(createMolten(0xFF000000 + color));
 		
