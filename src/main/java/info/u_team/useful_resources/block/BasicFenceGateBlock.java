91377b810ae763d7b153fc3cf7a768b9b5d80396
HyCraftHD <hycrafthd@live.de>
2020-11-29 18:26:46 +0100

    Add basic fence gate block

---
+++ src/main/java/info/u_team/useful_resources/block/BasicFenceGateBlock.java
@@ -0,0 +1,28 @@
+package info.u_team.useful_resources.block;
+
+import info.u_team.u_team_core.api.registry.IBlockItemProvider;
+import info.u_team.useful_resources.api.block.IBlockRenderType;
+import info.u_team.useful_resources.init.UsefulResourcesItemGroups;
+import net.minecraft.block.*;
+import net.minecraft.block.material.Material;
+import net.minecraft.item.*;
+import net.minecraftforge.common.ToolType;
+
+public class BasicFenceGateBlock extends FenceGateBlock implements IBlockItemProvider, IParticleBlock, IBlockRenderType {
+	
+	protected final BlockItem blockItem;
+	
+	public BasicFenceGateBlock(Rarity rarity, int harvestLevel, float hardness, float resistance) {
+		super(Properties.create(Material.IRON).sound(SoundType.METAL).harvestTool(ToolType.PICKAXE).harvestLevel(harvestLevel).setRequiresTool().hardnessAndResistance(hardness, resistance));
+		blockItem = createBlockItem(new Item.Properties().group(UsefulResourcesItemGroups.GROUP).rarity(rarity));
+	}
+	
+	protected BlockItem createBlockItem(Item.Properties blockItemProperties) {
+		return new BlockItem(this, blockItemProperties);
+	}
+	
+	@Override
+	public BlockItem getBlockItem() {
+		return blockItem;
+	}
+}
