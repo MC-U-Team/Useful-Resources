adb3364b1cd90c722b3582393fefa4f958d2b922
HyCraftHD <hycrafthd@live.de>
2020-12-06 19:21:39 +0100

    Add basic trap door block

---
+++ src/main/java/info/u_team/useful_resources/block/BasicTrapDoorBlock.java
@@ -0,0 +1,35 @@
+package info.u_team.useful_resources.block;
+
+import info.u_team.u_team_core.api.registry.IBlockItemProvider;
+import info.u_team.useful_resources.api.block.IBlockRenderType;
+import info.u_team.useful_resources.init.UsefulResourcesItemGroups;
+import net.minecraft.block.*;
+import net.minecraft.block.material.Material;
+import net.minecraft.item.*;
+import net.minecraftforge.api.distmarker.*;
+import net.minecraftforge.common.ToolType;
+
+public class BasicTrapDoorBlock extends TrapDoorBlock implements IBlockItemProvider, IParticleBlock, IBlockRenderType {
+	
+	protected final BlockItem blockItem;
+	
+	public BasicTrapDoorBlock(Rarity rarity, int harvestLevel, float hardness, float resistance) {
+		super(Properties.create(Material.IRON).sound(SoundType.METAL).harvestTool(ToolType.PICKAXE).harvestLevel(harvestLevel).setRequiresTool().hardnessAndResistance(hardness, resistance).notSolid().setAllowsSpawn((state, world, pos, type) -> false));
+		blockItem = createBlockItem(new Item.Properties().group(UsefulResourcesItemGroups.GROUP).rarity(rarity));
+	}
+	
+	protected BlockItem createBlockItem(Item.Properties blockItemProperties) {
+		return new BlockItem(this, blockItemProperties);
+	}
+	
+	@Override
+	public BlockItem getBlockItem() {
+		return blockItem;
+	}
+	
+	@OnlyIn(Dist.CLIENT)
+	@Override
+	public BlockRenderType getType() {
+		return BlockRenderType.CUTOUT;
+	}
+}
