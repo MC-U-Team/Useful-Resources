67ff08b339a2d2c7cb6b5e84a1050e76b91e233b
HyCraftHD <hycrafthd@live.de>
2020-12-08 00:24:12 +0100

    Add forgeTagsCopy from blocks to item

--- src/data/java/info/u_team/useful_resources/data/util/TagGenerationUtil.java
+++ src/data/java/info/u_team/useful_resources/data/util/TagGenerationUtil.java
@@ -1,10 +1,13 @@
 package info.u_team.useful_resources.data.util;
 
-import java.util.function.Function;
+import java.util.function.*;
 
 import info.u_team.u_team_core.data.CommonTagsProvider.BetterBuilder;
+import info.u_team.u_team_core.util.TagUtil;
 import info.u_team.useful_resources.api.resource.IResource;
 import info.u_team.useful_resources.api.type.IResourceType;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
 import net.minecraft.tags.ITag.INamedTag;
 import net.minecraftforge.registries.IForgeRegistryEntry;
 
@@ -19,4 +22,14 @@ public class TagGenerationUtil {
 			}
 		}
 	}
+	
+	public static void forgeTagsCopy(BiConsumer<INamedTag<Block>, INamedTag<Item>> consumer, IResource resource, IResourceType<Block> type, Block block) {
+		if (type.hasTag()) {
+			consumer.accept(type.getTag(resource), TagUtil.fromBlockTag(type.getTag(resource)));
+		}
+		if (type.hasUnifyTag()) {
+			consumer.accept(type.getUnifyTag(), TagUtil.fromBlockTag(type.getUnifyTag()));
+		}
+	}
+	
 }
