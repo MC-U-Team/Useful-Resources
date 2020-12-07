3d031f25be0eae48fe4f86e06c591c803b38e410
HyCraftHD <hycrafthd@live.de>
2020-12-07 23:39:03 +0100

    Add forEach Registry Type methods

--- src/data/java/info/u_team/useful_resources/data/resource/GenerationResources.java
+++ src/data/java/info/u_team/useful_resources/data/resource/GenerationResources.java
@@ -3,8 +3,13 @@ package info.u_team.useful_resources.data.resource;
 import java.util.*;
 import java.util.function.Consumer;
 
+import info.u_team.u_team_core.util.TriConsumer;
 import info.u_team.useful_resources.api.ResourceRegistry;
 import info.u_team.useful_resources.api.resource.IResource;
+import info.u_team.useful_resources.api.type.*;
+import net.minecraft.block.Block;
+import net.minecraft.fluid.Fluid;
+import net.minecraft.item.Item;
 import net.minecraft.util.Util;
 
 public class GenerationResources {
@@ -17,7 +22,19 @@ public class GenerationResources {
 		return Collections.unmodifiableSet(RESOURCES);
 	}
 	
-	public static void forEach(Consumer<? super IResource> action) {
-		RESOURCES.forEach(action);
+	public static void forEach(Consumer<? super IResource> consumer) {
+		RESOURCES.forEach(consumer);
+	}
+	
+	public static void forEachBlock(TriConsumer<? super IResource, BlockResourceType, Block> consumer) {
+		RESOURCES.stream().forEach(resource -> resource.iterateRegistryBlocks((type, block) -> consumer.accept(resource, type, block)));
+	}
+	
+	public static void forEachFluid(TriConsumer<? super IResource, FluidResourceType, Fluid> consumer) {
+		RESOURCES.stream().forEach(resource -> resource.iterateRegistryFluids((type, fluid) -> consumer.accept(resource, type, fluid)));
+	}
+	
+	public static void forEachItem(TriConsumer<? super IResource, ItemResourceType, Item> consumer) {
+		RESOURCES.stream().forEach(resource -> resource.iterateRegistryItems((type, item) -> consumer.accept(resource, type, item)));
 	}
 }
