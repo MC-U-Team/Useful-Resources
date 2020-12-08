b29480fb4fbb4b9b0d53d8f26952d1361f352434
HyCraftHD <hycrafthd@live.de>
2020-12-08 01:21:34 +0100

    addMoreCommonTag Method in TagGenerationUtil

--- src/data/java/info/u_team/useful_resources/data/util/TagGenerationUtil.java
+++ src/data/java/info/u_team/useful_resources/data/util/TagGenerationUtil.java
@@ -1,14 +1,20 @@
 package info.u_team.useful_resources.data.util;
 
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.function.*;
+import java.util.stream.Stream;
 
 import info.u_team.u_team_core.data.CommonTagsProvider.BetterBuilder;
 import info.u_team.u_team_core.util.TagUtil;
+import info.u_team.useful_resources.api.registry.RegistryEntry;
 import info.u_team.useful_resources.api.resource.IResource;
 import info.u_team.useful_resources.api.type.IResourceType;
+import info.u_team.useful_resources.util.MoreCollectors;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.tags.ITag.INamedTag;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.registries.IForgeRegistryEntry;
 
 public class TagGenerationUtil {
@@ -29,6 +35,19 @@ public class TagGenerationUtil {
 		}
 		if (type.hasUnifyTag()) {
 			consumer.accept(type.getUnifyTag(), TagUtil.fromBlockTag(type.getUnifyTag()));
+		}
+	}
+	
+	@SafeVarargs
+	public static <T extends IForgeRegistryEntry<T>> void addMoreCommonTag(Map<IResourceType<T>, RegistryEntry<T>> entries, Function<INamedTag<T>, BetterBuilder<T>> builderFunction, Function<ResourceLocation, INamedTag<T>> tagFunction, IResource resource, ResourceLocation baseTag, IResourceType<T>... types) {
+		final Map<IResourceType<T>, Boolean> hasType = Stream.of(types).collect(MoreCollectors.toLinkedMap(Function.identity(), type -> entries.containsKey(type)));
+		if (hasType.containsValue(true)) {
+			final INamedTag<T> tag = tagFunction.apply(new ResourceLocation(baseTag.getNamespace(), baseTag.getPath() + "/" + resource.getName()));
+			final BetterBuilder<T> builder = builderFunction.apply(tag);
+			hasType.entrySet().stream().filter(entry -> entry.getValue().equals(true)).map(Entry::getKey).forEach(type -> {
+				builder.add(type.getTag(resource));
+			});
+			builderFunction.apply(tagFunction.apply(baseTag)).add(tag);
 		}
 	}
 	
