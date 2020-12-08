2a1cc0f9deaea8ee102bf9fe221b8cd0383035a6
HyCraftHD <hycrafthd@live.de>
2020-12-08 03:15:51 +0100

    Use static imports now to make it a bit more readable

--- src/data/java/info/u_team/useful_resources/data/provider/ResourceBlockTagsProvider.java
+++ src/data/java/info/u_team/useful_resources/data/provider/ResourceBlockTagsProvider.java
@@ -1,10 +1,10 @@
 package info.u_team.useful_resources.data.provider;
 
+import static info.u_team.useful_resources.api.type.BlockResourceType.*;
 import static info.u_team.useful_resources.data.util.TagGenerationUtil.*;
 
 import info.u_team.u_team_core.data.*;
 import info.u_team.u_team_core.util.TagUtil;
-import info.u_team.useful_resources.api.type.BlockResourceType;
 import info.u_team.useful_resources.data.resource.TagGenerationResources;
 import info.u_team.useful_resources.resources.Resources;
 import net.minecraft.block.Blocks;
@@ -24,30 +24,30 @@ public class ResourceBlockTagsProvider extends CommonBlockTagsProvider {
 		
 		// Generate more common tags
 		TagGenerationResources.forEach(resource -> {
-			moreCommonTags(resource.getBlocks(), this::getBuilder, TagUtil::createBlockTag, resource, new ResourceLocation("forge", "ores"), BlockResourceType.ORE, BlockResourceType.NETHER_ORE, BlockResourceType.END_ORE);
+			moreCommonTags(resource.getBlocks(), this::getBuilder, TagUtil::createBlockTag, resource, new ResourceLocation("forge", "ores"), ORE, NETHER_ORE, END_ORE);
 		});
 		
 		// Add existing vanilla blocks to the right tags
-		forgeTags(this::getBuilder, Resources.IRON, BlockResourceType.ORE, Blocks.IRON_ORE);
-		forgeTags(this::getBuilder, Resources.GOLD, BlockResourceType.ORE, Blocks.GOLD_ORE);
-		forgeTags(this::getBuilder, Resources.GOLD, BlockResourceType.NETHER_ORE, Blocks.NETHER_GOLD_ORE);
-		forgeTags(this::getBuilder, Resources.DIAMOND, BlockResourceType.ORE, Blocks.DIAMOND_ORE);
-		forgeTags(this::getBuilder, Resources.EMERALD, BlockResourceType.ORE, Blocks.EMERALD_ORE);
-		forgeTags(this::getBuilder, Resources.LAPIS, BlockResourceType.ORE, Blocks.LAPIS_ORE);
-		forgeTags(this::getBuilder, Resources.QUARTZ, BlockResourceType.NETHER_ORE, Blocks.NETHER_QUARTZ_ORE);
-		forgeTags(this::getBuilder, Resources.COAL, BlockResourceType.ORE, Blocks.COAL_ORE);
-		forgeTags(this::getBuilder, Resources.REDSTONE, BlockResourceType.ORE, Blocks.REDSTONE_ORE);
-		forgeTags(this::getBuilder, Resources.IRON, BlockResourceType.BARS, Blocks.IRON_BARS);
-		forgeTags(this::getBuilder, Resources.IRON, BlockResourceType.CHAIN, Blocks.CHAIN);
-		forgeTags(this::getBuilder, Resources.IRON, BlockResourceType.DOOR, Blocks.IRON_DOOR);
-		forgeTags(this::getBuilder, Resources.IRON, BlockResourceType.TRAPDOOR, Blocks.IRON_TRAPDOOR);
+		forgeTags(this::getBuilder, Resources.IRON, ORE, Blocks.IRON_ORE);
+		forgeTags(this::getBuilder, Resources.GOLD, ORE, Blocks.GOLD_ORE);
+		forgeTags(this::getBuilder, Resources.GOLD, NETHER_ORE, Blocks.NETHER_GOLD_ORE);
+		forgeTags(this::getBuilder, Resources.DIAMOND, ORE, Blocks.DIAMOND_ORE);
+		forgeTags(this::getBuilder, Resources.EMERALD, ORE, Blocks.EMERALD_ORE);
+		forgeTags(this::getBuilder, Resources.LAPIS, ORE, Blocks.LAPIS_ORE);
+		forgeTags(this::getBuilder, Resources.QUARTZ, NETHER_ORE, Blocks.NETHER_QUARTZ_ORE);
+		forgeTags(this::getBuilder, Resources.COAL, ORE, Blocks.COAL_ORE);
+		forgeTags(this::getBuilder, Resources.REDSTONE, ORE, Blocks.REDSTONE_ORE);
+		forgeTags(this::getBuilder, Resources.IRON, BARS, Blocks.IRON_BARS);
+		forgeTags(this::getBuilder, Resources.IRON, CHAIN, Blocks.CHAIN);
+		forgeTags(this::getBuilder, Resources.IRON, DOOR, Blocks.IRON_DOOR);
+		forgeTags(this::getBuilder, Resources.IRON, TRAPDOOR, Blocks.IRON_TRAPDOOR);
 		
 		// Add to the existing vanilla tags
 		TagGenerationResources.forEach(resource -> {
-			conditionTags(resource.getBlocks(), this::getBuilder, BlockResourceType.FENCE, BlockTags.FENCES);
-			conditionTags(resource.getBlocks(), this::getBuilder, BlockResourceType.FENCE_GATE, BlockTags.FENCE_GATES);
-			conditionTags(resource.getBlocks(), this::getBuilder, BlockResourceType.DOOR, BlockTags.DOORS);
-			conditionTags(resource.getBlocks(), this::getBuilder, BlockResourceType.TRAPDOOR, BlockTags.TRAPDOORS);
+			conditionTags(resource.getBlocks(), this::getBuilder, FENCE, BlockTags.FENCES);
+			conditionTags(resource.getBlocks(), this::getBuilder, FENCE_GATE, BlockTags.FENCE_GATES);
+			conditionTags(resource.getBlocks(), this::getBuilder, DOOR, BlockTags.DOORS);
+			conditionTags(resource.getBlocks(), this::getBuilder, TRAPDOOR, BlockTags.TRAPDOORS);
 		});
 	}
 }
--- src/data/java/info/u_team/useful_resources/data/provider/ResourceItemTagsProvider.java
+++ src/data/java/info/u_team/useful_resources/data/provider/ResourceItemTagsProvider.java
@@ -1,10 +1,11 @@
 package info.u_team.useful_resources.data.provider;
 
+import static info.u_team.useful_resources.api.type.BlockResourceType.*;
+import static info.u_team.useful_resources.api.type.ItemResourceType.*;
 import static info.u_team.useful_resources.data.util.TagGenerationUtil.*;
 
 import info.u_team.u_team_core.data.*;
 import info.u_team.u_team_core.util.TagUtil;
-import info.u_team.useful_resources.api.type.*;
 import info.u_team.useful_resources.data.resource.TagGenerationResources;
 import info.u_team.useful_resources.resources.Resources;
 import net.minecraft.item.Items;
@@ -25,30 +26,30 @@ public class ResourceItemTagsProvider extends CommonItemTagsProvider {
 		
 		// Generate more common tags
 		TagGenerationResources.forEach(resource -> {
-			moreCommonTagsCopy(this::copy, resource, new ResourceLocation("forge", "ores"), BlockResourceType.ORE, BlockResourceType.NETHER_ORE, BlockResourceType.END_ORE);
-			moreCommonTags(resource.getItems(), this::getBuilder, TagUtil::createItemTag, resource, new ResourceLocation("forge", "crushed_ores"), ItemResourceType.CRUSHED_ORE, ItemResourceType.CRUSHED_NETHER_ORE, ItemResourceType.CRUSHED_END_ORE);
+			moreCommonTagsCopy(this::copy, resource, new ResourceLocation("forge", "ores"), ORE, NETHER_ORE, END_ORE);
+			moreCommonTags(resource.getItems(), this::getBuilder, TagUtil::createItemTag, resource, new ResourceLocation("forge", "crushed_ores"), CRUSHED_ORE, CRUSHED_NETHER_ORE, CRUSHED_END_ORE);
 		});
 		
 		// Generate more common unify tags
-		moreCommonUnifyTags(this::getBuilder, TagUtil::createItemTag, new ResourceLocation("forge", "tools"), ItemResourceType.AXE, ItemResourceType.HOE, ItemResourceType.PICKAXE, ItemResourceType.SHOVEL, ItemResourceType.SWORD);
-		moreCommonUnifyTags(this::getBuilder, TagUtil::createItemTag, new ResourceLocation("forge", "armors"), ItemResourceType.HELMET, ItemResourceType.CHESTPLATE, ItemResourceType.LEGGINGS, ItemResourceType.BOOTS);
+		moreCommonUnifyTags(this::getBuilder, TagUtil::createItemTag, new ResourceLocation("forge", "tools"), AXE, HOE, PICKAXE, SHOVEL, SWORD);
+		moreCommonUnifyTags(this::getBuilder, TagUtil::createItemTag, new ResourceLocation("forge", "armors"), HELMET, CHESTPLATE, LEGGINGS, BOOTS);
 		
 		// Add existing vanilla blocks / items to the right tags
-		forgeTagsCopy(this::copy, Resources.IRON, BlockResourceType.ORE);
-		forgeTagsCopy(this::copy, Resources.GOLD, BlockResourceType.ORE);
-		forgeTagsCopy(this::copy, Resources.GOLD, BlockResourceType.NETHER_ORE);
-		forgeTagsCopy(this::copy, Resources.DIAMOND, BlockResourceType.ORE);
-		forgeTagsCopy(this::copy, Resources.EMERALD, BlockResourceType.ORE);
-		forgeTagsCopy(this::copy, Resources.LAPIS, BlockResourceType.ORE);
-		forgeTagsCopy(this::copy, Resources.QUARTZ, BlockResourceType.NETHER_ORE);
-		forgeTagsCopy(this::copy, Resources.COAL, BlockResourceType.ORE);
-		forgeTagsCopy(this::copy, Resources.REDSTONE, BlockResourceType.ORE);
-		forgeTagsCopy(this::copy, Resources.IRON, BlockResourceType.BARS);
-		forgeTagsCopy(this::copy, Resources.IRON, BlockResourceType.CHAIN);
-		forgeTagsCopy(this::copy, Resources.IRON, BlockResourceType.DOOR);
-		forgeTagsCopy(this::copy, Resources.IRON, BlockResourceType.TRAPDOOR);
+		forgeTagsCopy(this::copy, Resources.IRON, ORE);
+		forgeTagsCopy(this::copy, Resources.GOLD, ORE);
+		forgeTagsCopy(this::copy, Resources.GOLD, NETHER_ORE);
+		forgeTagsCopy(this::copy, Resources.DIAMOND, ORE);
+		forgeTagsCopy(this::copy, Resources.EMERALD, ORE);
+		forgeTagsCopy(this::copy, Resources.LAPIS, ORE);
+		forgeTagsCopy(this::copy, Resources.QUARTZ, NETHER_ORE);
+		forgeTagsCopy(this::copy, Resources.COAL, ORE);
+		forgeTagsCopy(this::copy, Resources.REDSTONE, ORE);
+		forgeTagsCopy(this::copy, Resources.IRON, BARS);
+		forgeTagsCopy(this::copy, Resources.IRON, CHAIN);
+		forgeTagsCopy(this::copy, Resources.IRON, DOOR);
+		forgeTagsCopy(this::copy, Resources.IRON, TRAPDOOR);
 		
-		forgeTags(this::getBuilder, Resources.COAL, ItemResourceType.GEM, Items.COAL);
+		forgeTags(this::getBuilder, Resources.COAL, GEM, Items.COAL);
 		
 		// Add to the existing vanilla tags
 		copy(BlockTags.FENCES, ItemTags.FENCES);
