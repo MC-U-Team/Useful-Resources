6f1eecf959c6caa485e0707b8f74af58441b8118
HyCraftHD <hycrafthd@live.de>
2020-12-10 19:55:50 +0100

    Use that in the languages provider

--- src/data/java/info/u_team/useful_resources/data/provider/ResourceLanguagesProvider.java
+++ src/data/java/info/u_team/useful_resources/data/provider/ResourceLanguagesProvider.java
@@ -9,6 +9,7 @@ import java.util.stream.*;
 
 import info.u_team.u_team_core.data.*;
 import info.u_team.useful_resources.data.resource.GenerationResources;
+import info.u_team.useful_resources.data.util.LanguageGenerationDecider;
 import info.u_team.useful_resources.init.UsefulResourcesItemGroups;
 import net.minecraftforge.registries.ForgeRegistryEntry;
 
@@ -22,45 +23,68 @@ public class ResourceLanguagesProvider extends CommonLanguagesProvider {
 	public void addTranslations() {
 		add(UsefulResourcesItemGroups.GROUP, "Useful Resources");
 		
+		final LanguageGenerationDecider languageDecider = LanguageGenerationDecider.create();
+		
+		languageDecider.addSpecialBlock(NETHER_ORE, (resource, type, block, name, configurator) -> {
+			add(block, "Nether " + name + " Ore");
+		});
+		languageDecider.addSpecialBlock(END_ORE, (resource, type, block, name, configurator) -> {
+			add(block, "End " + name + " Ore");
+		});
+		languageDecider.addSpecialBlock(BLOCK, (resource, type, block, name, configurator) -> {
+			add(block, "Block of " + name);
+		});
+		languageDecider.addSpecialBlock(MOLTEN_FLUID, (resource, type, block, name, configurator) -> {
+			add(block, "Molten " + name);
+		});
+		
+		languageDecider.addSpecialFluid(MOLTEN, (resource, type, fluid, name, configurator) -> {
+			add(fluid, "Molten " + name);
+		});
+		languageDecider.addSpecialFluid(MOLTEN_FLOWING, (resource, type, fluid, name, configurator) -> {
+			add(fluid, "Molten " + name);
+		});
+		
+		languageDecider.addSpecialItem(CRUSHED_ORE, (resource, type, item, name, configurator) -> {
+			add(item, "Crushed " + name + " Ore");
+		});
+		languageDecider.addSpecialItem(CRUSHED_NETHER_ORE, (resource, type, item, name, configurator) -> {
+			add(item, "Crushed Nether " + name + " Ore");
+		});
+		languageDecider.addSpecialItem(CRUSHED_END_ORE, (resource, type, item, name, configurator) -> {
+			add(item, "Crushed End " + name + " Ore");
+		});
+		languageDecider.addSpecialItem(PURE_CRUSHED_ORE, (resource, type, item, name, configurator) -> {
+			add(item, "Pure Crushed " + name + " Ore");
+		});
+		languageDecider.addSpecialItem(DENSE_PLATE, (resource, type, item, name, configurator) -> {
+			add(item, "Dense " + name + " Plate");
+		});
+		languageDecider.addSpecialItem(MOLTEN_BUCKET, (resource, type, item, name, configurator) -> {
+			add(item, "Molten " + name + " Bucket");
+		});
+		
 		GenerationResources.forEach(resource -> {
+			
 			final String name = capitalize(resource.getName().replace("_", " "));
 			final Function<ForgeRegistryEntry<?>, String> defaultNameFunction = entry -> capitalize(entry.getRegistryName().getPath().replace("_", " "));
+			
 			resource.iterateRegistryBlocks((type, block) -> {
-				if (type == NETHER_ORE) {
-					add(block, "Nether " + name + " Ore");
-				} else if (type == END_ORE) {
-					add(block, "End " + name + " Ore");
-				} else if (type == BLOCK) {
-					add(block, "Block of " + name);
-				} else if (type == MOLTEN_FLUID) {
-					add(block, "Molten " + name);
-				} else {
-					add(block, defaultNameFunction.apply(block));
-				}
+				languageDecider.generate(resource, type, block, name, resource.getDataGeneratorConfigurator(), entry -> {
+					add(entry, defaultNameFunction.apply(block));
+				});
 			});
+			
 			resource.iterateRegistryFluids((type, fluid) -> {
-				if (type == MOLTEN || type == MOLTEN_FLOWING) {
-					add(fluid, "Molten " + name);
-				} else {
-					add(fluid, defaultNameFunction.apply(fluid));
-				}
+				languageDecider.generate(resource, type, fluid, name, resource.getDataGeneratorConfigurator(), entry -> {
+					add(entry, defaultNameFunction.apply(fluid));
+				});
 			});
+			
 			resource.iterateRegistryItems((type, item) -> {
-				if (type == CRUSHED_ORE) {
-					add(item, "Crushed " + name + " Ore");
-				} else if (type == CRUSHED_NETHER_ORE) {
-					add(item, "Crushed Nether " + name + " Ore");
-				} else if (type == CRUSHED_END_ORE) {
-					add(item, "Crushed End " + name + " Ore");
-				} else if (type == PURE_CRUSHED_ORE) {
-					add(item, "Pure Crushed " + name + " Ore");
-				} else if (type == DENSE_PLATE) {
-					add(item, "Dense " + name + " Plate");
-				} else if (type == MOLTEN_BUCKET) {
-					add(item, "Molten " + name + " Bucket");
-				} else {
-					add(item, defaultNameFunction.apply(item));
-				}
+				languageDecider.generate(resource, type, item, name, resource.getDataGeneratorConfigurator(), entry -> {
+					add(entry, defaultNameFunction.apply(item));
+				});
 			});
 		});
 		
