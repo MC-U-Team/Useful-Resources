651257cdc630b04db8edc81c60a28ec97da14a60
HyCraftHD <hycrafthd@live.de>
2020-12-12 16:30:30 +0100

    Use the resource data type now to encode the normal and tiny resource types

--- src/api/java/info/u_team/useful_resources/api/resource/data/IResourceDataType.java
+++ src/api/java/info/u_team/useful_resources/api/resource/data/IResourceDataType.java
@@ -1,7 +1,14 @@
 package info.u_team.useful_resources.api.resource.data;
 
+import info.u_team.useful_resources.api.type.IResourceType;
+import net.minecraft.util.IItemProvider;
+
 public interface IResourceDataType {
 	
 	String getName();
+	
+	IResourceType<? extends IItemProvider> getNormalResourceType();
+	
+	IResourceType<? extends IItemProvider> getTinyResourceType();
 	
 }
