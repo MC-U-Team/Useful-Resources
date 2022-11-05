942e2fdc662bef43b164681db5d96feb64b0e4bf
HyCraftHD <hycrafthd@live.de>
2022-11-05 22:37:08 +0100

    Cleanup

--- src/api/java/info/u_team/useful_resources/api/Resource.java
+++
@@ -1,7 +0,0 @@
-package info.u_team.useful_resources.api;
-
-public interface Resource {
-	
-	String getName();
-	
-}
--- src/main/java/info/u_team/useful_resources/init/UsefulResourcesCommonConstruct.java
+++ src/main/java/info/u_team/useful_resources/init/UsefulResourcesCommonConstruct.java
@@ -2,7 +2,6 @@ package info.u_team.useful_resources.init;
 
 import info.u_team.u_team_core.api.construct.Construct;
 import info.u_team.u_team_core.api.construct.ModConstruct;
-import info.u_team.u_team_core.util.registry.BusRegister;
 import info.u_team.useful_resources.UsefulResourcesMod;
 
 @Construct(modid = UsefulResourcesMod.MODID)
@@ -10,7 +9,6 @@ public class UsefulResourcesCommonConstruct implements ModConstruct {
 	
 	@Override
 	public void construct() {
-		BusRegister.registerMod(Resources::registerMod);
 	}
 	
 }
