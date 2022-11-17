f80147329ee084771918b526c2393cf359f17e91
HyCraftHD <hycrafthd@live.de>
2022-11-17 16:24:43 +0100

    Remove caching, not useful in this context

--- src/api/java/info/u_team/useful_resources/api/registry/ResourceTypeKey.java
+++ src/api/java/info/u_team/useful_resources/api/registry/ResourceTypeKey.java
@@ -1,23 +1,13 @@
 package info.u_team.useful_resources.api.registry;
 
-import java.util.Collections;
-import java.util.Map;
 import java.util.Objects;
 
-import com.google.common.collect.Maps;
-
-import info.u_team.u_team_core.util.CastUtil;
 import info.u_team.useful_resources.api.resource.AbstractResourceType;
 
 public final class ResourceTypeKey<T> {
 	
-	private static final Map<String, ResourceTypeKey<?>> VALUES = Collections.synchronizedMap(Maps.newIdentityHashMap());
-	
 	public static <T> ResourceTypeKey<T> create(AbstractResourceType<T> type) {
-		final String name = type.getName().intern();
-		return CastUtil.uncheckedCast(VALUES.computeIfAbsent(name, (unused) -> {
-			return new ResourceTypeKey<>(type);
-		}));
+		return new ResourceTypeKey<>(type);
 	}
 	
 	private final AbstractResourceType<T> type;
