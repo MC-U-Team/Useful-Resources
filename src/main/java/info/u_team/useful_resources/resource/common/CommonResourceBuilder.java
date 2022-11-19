8d9b03f1cf97d80646e3277b00dfbc240f076efb
HyCraftHD <hycrafthd@live.de>
2022-11-19 15:17:37 +0100

    Remove test

--- src/main/java/info/u_team/useful_resources/resource/common/CommonResourceBuilder.java
+++ src/main/java/info/u_team/useful_resources/resource/common/CommonResourceBuilder.java
@@ -12,11 +12,6 @@ public class CommonResourceBuilder {
 		return new BasicOreResourceBuilder(name, color, rarity, existingTypes);
 	}
 	
-	public static String test() {
-		System.out.println("CALLED FROM JS");
-		return "ROFL";
-	}
-	
 	// Utility methods
 	
 	public static Consumer<ExistingResourceTypes> noExisting() {
